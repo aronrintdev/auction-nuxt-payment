2f47fc63fbfe185904535749dfcd732828c29d8a
Arslan Javaid <arsl.javaid@gmail.com>
2022-09-18 16:06:34 -0500

    Chore: fix pipeline and deployment

--- .platform/confighooks/postdeploy/02_deploycode_job.sh
+++ .platform/confighooks/postdeploy/02_deploycode_job.sh
@@ -4,5 +4,5 @@ npm install
 npm install @nuxt/bridge-edge@3.0.0-27272804.ca557e7
 npm install nuxt3 -g
 npm run build
-node_modules/pm2/bin/pm2 delete admin
+node_modules/pm2/bin/pm2 delete index
 npm run start
--- .platform/hooks/postdeploy/09_deploycode_job.sh
+++ .platform/hooks/postdeploy/09_deploycode_job.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /var/app/current
-npm install pm2 -g
 npm install
 npm install @nuxt/bridge-edge@3.0.0-27272804.ca557e7
 npm install nuxt3 -g
 npm run build
-pm2 delete index
+node_modules/pm2/bin/pm2 delete index
 npm run start
