07004fa6592a595095b3b96a05613ea214f51b3c
Arslan Javaid <arsl.javaid@gmail.com>
2022-09-19 11:40:06 -0500

    Chore: push new changes

--- .platform/hooks/prebuild/11_deploycode_job.sh
+++ .platform/hooks/prebuild/11_deploycode_job.sh
@@ -2,7 +2,3 @@
 sudo mkdir /var/app/current
 cd /var/app/current
 sudo cp -R /var/app/staging/* /var/app/current -f
-sudo su
-sudo npm install pm2 -g
-sudo npm install deasync -g
-sudo npm install node-gyp -g
