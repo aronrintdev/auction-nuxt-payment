51609bb75aa90cece4609ec010e0a8f901114326
Babar Saleh Hayat <babar.dev@deadstock.co>
2022-09-20 00:38:25 +0500

    fixed gitignore

--- .nuxt/clipboard.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueClipboard from 'vue-clipboard2'
-
-const { autoSetContainer = false } = {} || {}
-
-VueClipboard.config.autoSetContainer = autoSetContainer
-Vue.use(VueClipboard)
