05cda06e928876cc4ccc57ee12748894fd2a46cf
Atif Rehman <atif.dev@deadstock.co>
2022-09-27 02:00:40 -0600

    created dynamic page functionality

---
+++ pages/_page/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-row class="vh-100 mt-5">
+    <b-col md="12" class="text-center">
+      <!-- TODO: To be filled once content is ready -->
+      <span>{{ link }}</span>
+    </b-col>
+  </b-row>
+</template>
+<script>
+export default {
+  name: 'TermsAndConditions',
+  layout: 'IndexLayout',
+  data() {
+    return {
+      link: '',
+    }
+  },
+  mounted() {
+    this.link = this.$route.params.link
+    this.getPageData(this.link)
+  },
+  methods: {
+    getPageData(link) {
+      /** Todo need to call api to get page data from database */
+      console.log(link.replace('-', '_'))
+    },
+  },
+}
+</script>
