a95cda78b860c2194b1d658ae8159ac35ae8c902
aronrint <aronrint@gmail.com>
2022-10-07 22:43:33 +0300

    Shopping cart store module actions
    - Updated the shopping cart store module actions and added a helper method to increment product quantity or add product.

--- store/shopping-cart/actions.js
+++ store/shopping-cart/actions.js
@@ -17,6 +17,26 @@ export function addProduct({ commit, state }, product) {
 }
 
 /**
+ * Add product to shopping cart or increment its quantity if exists.
+ * @param commit
+ * @param state
+ * @param product
+ */
+export function addOrIncrementQuantityProduct({ commit, state }, product) {
+  const found = state.shoppingCart.find((p) => {
+    return p.listing_item_id === product.listing_item_id
+      && p.price === product.price
+      && p.size.id === product.size.id
+  })
+
+  if (!found) {
+    commit('addProduct', product)
+  } else {
+    commit('incrementQuantity', found)
+  }
+}
+
+/**
  * Add alternative products to the shopping cart.
  * @param commit
  * @param products
