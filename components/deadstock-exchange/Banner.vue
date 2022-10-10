a55a87ed6b06721c69a45cce3627aa2f67d305f3
khawarjavaid <90414577+khawarjavaid@users.noreply.github.com>
2022-10-11 00:50:29 +0500

    basic pages

---
+++ components/deadstock-exchange/Banner.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="fade">
+    <div class="col-12 px-0 text-right position-relative promotions-banner">
+      <img
+          :src="bannerImageUrl"
+          class="banner-image"
+      />
+      <div class="position-absolute w-100 promotions-banner-overlay">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 text-left promotions-banner-title">{{ title || $t('promotions.promotions') }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'PromotionsBanner',
+  props: {
+    title: {
+      type: [String, null],
+      default: null,
+    },
+    bannerImage: {
+      type: [String, null],
+      default: null,
+    }
+  },
+  computed: {
+    bannerImageUrl() {
+      return this.bannerImage || require('~/assets/img/promotions/promotions-banner1.png')
+    }
+  }
+};
+</script>
