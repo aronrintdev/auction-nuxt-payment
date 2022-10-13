329de6a932de0b1e16f61511ea1f3838b1668610
shazim Alim <shazim96khan@gmail.com>
2022-10-13 22:36:12 +0500

    shazim change

---
+++ pages/stock/exchange/_type/detail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid promotions">
+        <div class="container container-auction-details">
+          <div class="row ">
+            <div class="col-12 col-md-11 col-sm-11 mx-auto">
+            <h3>Detail </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+  </template>
+  <script>
+  
+  export default {
+    layout(context) {
+      return context.$auth.user ? 'Profile' : 'IndexLayout'
+    }
+  }
+  </script>
+  
