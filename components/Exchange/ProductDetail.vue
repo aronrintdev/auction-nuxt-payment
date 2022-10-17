a86e4fdc9e93a5dae6bcbdf21c73211a2f7c29b1
shazim Alim <shazim96khan@gmail.com>
2022-10-18 00:59:56 +0500

    Complete Detail page design

--- components/Exchange/ProductDetail.vue
+++ components/Exchange/ProductDetail.vue
@@ -1,52 +1,112 @@
 <template>
-  <div>
+  <div class="exchange-product-detail">
     <!-- Loaders -->
-      <div
+    <div
       v-if="loading"
       class="d-flex align-items-center justify-content-center h-500"
     >
       <Loader :loading="loading"></Loader>
     </div>
     <div class="row">
-        <div class="col-lg-6 col-md-12 col-sm-12">
-          <div class="row">
-            <div class="col-lg-2">
-              <ProductThumb
-                src="https://api.deadstock.co/prod/api/sneakers/CV7562-401/image"
+      <div class="col-lg-6 col-md-12 col-sm-12">
+        <div class="row">
+          <div class="col-lg-2">
+            <ProductThumb
+              src="https://api.deadstock.co/prod/api/sneakers/CV7562-401/image"
+              class="product-thumbnail"
+            />
+          </div>
+          <div class="col-lg-5">
+            <div class="body-4-bold mb-2">Nike Lebron 18 Low (2021)</div>
+            <div class="body-4-normal mb-2 text-gray-6">Black & White</div>
+          </div>
+          <div class="col-lg-5 text-right">
+            <Button variant="dark-blue" class="mx-3" @click="handleBuyClick">
+              <div class="d-flex justify-content-between">
+                <div>{{ $t('deadstock_exchange.detail.buy') }}</div>
+              </div>
+            </Button>
+            <Button variant="dark" @click="handleSellClick">
+              <div class="d-flex justify-content-between">
+                <div>{{ $t('deadstock_exchange.detail.sell') }}</div>
+              </div>
+            </Button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="body-4-normal mb-2 text-gray-6">
+              {{ dayjs().format('DD, MMM, h:mm A') }}
+            </div>
+            <div class="body-4-bold mb-2">$ 250.00</div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="body-4-normal mb-2 text-success">+0.64 (+0.36%)</div>
+          </div>
+          <div class="col-lg-6">
+            <div class="body-4-normal mb-2 text-gray-6">
+              <NavGroup
+                :data="categoryItems"
+                :value="current"
+                class="section-nav"
+                nav-key="line_chart"
+                @change="handleTabChange"
               />
             </div>
-            <div class="col-lg-5">
-              <div class="body-4-bold mb-2">Nike Lebron 18 Low (2021)</div>
-              <div class="body-4-normal mb-2 text-gray-6">
-                Black & White
-              </div>
-            </div>
-            <div class="col-lg-5 text-right">
-              <Button
-                  variant="dark-blue"
-                  class="mx-3"
-                  @click="handleSellNowClick"
-                >
-                  <div class="d-flex justify-content-between">
-                    <div>{{ $t('deadstock_exchange.detail.buy') }} </div>
-                  </div>
-                </Button>
-                <Button
-                  variant="dark"
-                  @click="handleSellNowClick"
-                >
-                  <div class="d-flex justify-content-between">
-                    <div>{{ $t('deadstock_exchange.detail.sell') }} </div>
-                  </div>
-                </Button>
+          </div>
+          <div class="col-lg-3">
+            <div class="rounded-circle shadow share-icon">
+              <ShareIcon />
             </div>
           </div>
         </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="body-4-normal mb-2 text-gray-6">
-          {{ dayjs() | diffForHumans }}
+        <div class="row">
+          <div class="col-lg-12">
+            <LineChart
+              :chart-data="lineDatasets"
+              :options="lineChartOptions"
+              class="line-chart"
+              chart-id="vendor-dashboard-line-chart"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-12 col-sm-12">
+        <div class="body-2-bold mb-2">
+          {{ $t('deadstock_exchange.detail.similar_products') }}
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <!-- Input search -->
+            <div class="form-group d-flex align-items-center m-0">
+              <img
+                :src="require('~/assets/img/icons/search.svg')"
+                class="icon-search"
+                alt="Search"
+              />
+              <input
+                id="search-result"
+                v-model="searchValue"
+                type="text"
+                class="form-control form-input vd-purchases-browse-input"
+                :placeholder="
+                  $t('deadstock_exchange.detail.similar_search_placeholder')
+                "
+                autocomplete="on"
+                @input="searchProduct"
+              />
+            </div>
+            <!-- ./Input search -->
+          </div>
+          <div class="col-lg-4"></div>
+          <div class="row">
+            <div class="col-ld-12">
+              <!-- ProducListCard Table -->
+              <SimilarProductTable :products="products" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -55,16 +115,149 @@
 
 <script>
 import dayjs from 'dayjs'
-import {Button, Loader } from '~/components/common'
+import SimilarProductTable from './SimilarProductTable.vue'
+import { Button, Loader } from '~/components/common'
 import ProductThumb from '~/components/product/Thumb.vue'
+import NavGroup from '~/components/common/NavGroup.vue'
+import ShareIcon from '~/assets/img/icons/share.svg?inline'
+
 export default {
-  name:'ProductDetail',
-  components: { ProductThumb,Button,Loader},
-  data(){
+  name: 'ProductDetail',
+  components: {
+    ProductThumb,
+    Button,
+    Loader,
+    NavGroup,
+    ShareIcon,
+    SimilarProductTable,
+  },
+  data() {
     return {
       dayjs,
-      loading:false
+      loading: false,
+      products: [],
+      categoryItems: [
+        {
+          label: this.$t('deadstock_exchange.detail.24h'),
+          value: '24',
+        },
+        {
+          label: this.$t('deadstock_exchange.detail.7d'),
+          value: '7',
+        },
+        {
+          label: this.$t('deadstock_exchange.detail.30d'),
+          value: '30',
+        },
+        {
+          label: this.$t('deadstock_exchange.detail.6m'),
+          value: '6',
+        },
+        {
+          label: this.$t('deadstock_exchange.detail.1y'),
+          value: '1',
+        },
+        {
+          label: this.$t('deadstock_exchange.detail.all'),
+          value: 'all',
+        },
+      ],
+      current: '24',
+      // line chart data
+      lineChartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                drawOnChartArea: false,
+              },
+              ticks: {
+                fontFamily: 'Montserrat',
+                fontColor: '#000',
+                fontSize: 12,
+                fontStyle: 'bold',
+              },
+            },
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                labelString: [
+                  '$0.00',
+                  '$100.00',
+                  '$200.00',
+                  '$300.00',
+                  '$400.00',
+                ],
+              },
+              gridLines: {
+                drawOnChartArea: false,
+              },
+              ticks: {
+                fontFamily: 'Montserrat',
+                fontColor: '#000',
+                fontSize: 12,
+                fontStyle: 'bold',
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+        elements: {
+          point: {
+            radius: 0,
+          },
+        },
+      },
+      lineDatasets: {
+        labels: ['2 pm', '6 pm', '10 pm', '2 am', '6 am', '10 am', '2 pm'],
+        datasets: [
+          {
+            borderColor: '#0bb63f',
+            backgroundColor: null,
+            data: [0, 50.0, 150.0, 250.0, 400.0],
+            fill: false,
+            borderWidth: 2,
+          },
+        ],
+      },
     }
-  }
+  },
+  mounted() {
+    this.loadPage()
+  },
+  methods: {
+    handleTabChange(category) {
+      this.current = category
+    },
+    handleBuyClick() {},
+    handleSellClick() {},
+    loadPage() {
+      this.$axios
+        .get('/products', {
+          // .get('/stock-exchange', {
+          params: {
+            type: this.type,
+            page: this.currentPage,
+            take: this.perPage,
+            ...this.filter,
+          },
+        })
+        .then((response) => {
+          if (response.data) {
+            console.log(response.data)
+            this.products = response.data.data
+            // this.totalRows = response.data.total
+          }
+        })
+        .catch((error) => {
+          this.$toasted.error(error.message)
+        })
+    },
+  },
 }
 </script>
---
+++ components/Exchange/SimilarProductTable.vue
@@ -0,0 +1,367 @@
+<template>
+    <table class="table trend-table-wrapper">
+      <tbody>
+        <tr
+          v-for="(product, index) in products"
+          :key="`product-${product.id}-trend`"
+        >
+          <td></td>
+          <td class="col-no font-primary">{{ index + 1 }}</td>
+          <td class="col-thumb">
+            <div><ProductThumb :product="product" /></div>
+          </td>
+          <td class="col-product-detail">
+            <div class="text-name font-primary">{{ product.name }}</div>
+            <div class="text-color font-primary">{{ product.colorway }}</div>
+          </td>
+          <td class="col-price d-none d-sm-table-cell">
+            {{ product.last_price | toCurrency }}
+          </td>
+          <td
+            :class="`col-trend-${
+              product.trend_24h >= 0 ? 'positive' : 'negative'
+            }`"
+            class="d-none d-sm-table-cell"
+          >
+            {{ product.trend_24h | toPercentage }}
+          </td>
+          <td
+            :class="`col-trend-${
+              product.trend_7d >= 0 ? 'positive' : 'negative'
+            }`"
+            class="d-none d-sm-table-cell"
+          >
+            {{ product.trend_7d | toPercentage }}
+          </td>
+          <td
+            :class="`col-trend-${
+              product.trend_30d >= 0 ? 'positive' : 'negative'
+            }`"
+            class="d-none d-sm-table-cell"
+          >
+            {{ product.trend_30d | toPercentage }}
+          </td>
+          <td class="col-graph d-none d-sm-table-cell">
+            <LineChart
+              :chart-data="product.last_7d_data"
+              :options="chartOptions"
+              class="trend-graph"
+            ></LineChart>
+          </td>
+          <td class="d-table-cell d-sm-none text-right">
+            <div class="col-price pricing font-primary">
+              {{ product.last_price | toCurrency }}
+            </div>
+            <div
+              class="font-primary pricing"
+              :class="`col-trend-${
+                product.trend_24h >= 0 ? 'positive' : 'negative'
+              }`"
+            >
+              {{ product.trend_24h | toPercentage }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  <script>
+  import ProductThumb from '~/components/product/Thumb.vue'
+  export default {
+    name: 'ProductTrendListCard',
+    components: { ProductThumb },
+    props: {
+      products: {
+        type: Array,
+        default: () => {
+          return [
+            {
+              id: 1,
+              name: 'Yeezy Boost 350 V2',
+              category: 'sneakers',
+              sku: 'GY3438',
+              colorway: 'Black & White',
+              trend_24h: -10.5,
+              trend_7d: -10.5,
+              trend_30d: -12.5,
+              last_price: 23000,
+              last_7d_data: {
+                labels: [
+                  new Date('2022-2-8 03:24:00'),
+                  new Date('2022-2-9 03:24:00'),
+                  new Date('2022-2-10 03:24:00'),
+                  new Date('2022-2-11 03:24:00'),
+                  new Date('2022-2-12 03:24:00'),
+                  new Date('2022-2-13 03:24:00'),
+                  new Date('2022-2-14 03:24:00'),
+                ],
+                datasets: [
+                  {
+                    borderColor: '#d93730',
+                    backgroundColor: null,
+                    data: [0, 30, 200, 100, 280, 100, 400],
+                    fill: false,
+                    borderWidth: 2,
+                  },
+                ],
+              },
+            },
+            {
+              id: 2,
+              name: 'Nike Dunk Low (2021)',
+              category: 'sneakers',
+              sku: 'DC6515-100',
+              colorway: 'Sesame',
+              trend_24h: 10.5,
+              trend_7d: 10.5,
+              trend_30d: 12.5,
+              last_price: 23000,
+              last_7d_data: {
+                labels: [
+                  new Date('2022-2-8 03:24:00'),
+                  new Date('2022-2-9 03:24:00'),
+                  new Date('2022-2-10 03:24:00'),
+                  new Date('2022-2-11 03:24:00'),
+                  new Date('2022-2-12 03:24:00'),
+                  new Date('2022-2-13 03:24:00'),
+                  new Date('2022-2-14 03:24:00'),
+                ],
+                datasets: [
+                  {
+                    borderColor: '#0bb63f',
+                    backgroundColor: null,
+                    data: [0, 30, 200, 100, 280, 100, 400],
+                    fill: false,
+                    borderWidth: 2,
+                  },
+                ],
+              },
+            },
+            {
+              id: 3,
+              name: 'Jordan 4 Retro (2021)',
+              category: 'sneakers',
+              sku: 'CW6024-600',
+              colorway: 'University Blue',
+              trend_24h: -10.5,
+              trend_7d: -10.5,
+              trend_30d: -12.5,
+              last_price: 23000,
+              last_7d_data: {
+                labels: [
+                  new Date('2022-2-8 03:24:00'),
+                  new Date('2022-2-9 03:24:00'),
+                  new Date('2022-2-10 03:24:00'),
+                  new Date('2022-2-11 03:24:00'),
+                  new Date('2022-2-12 03:24:00'),
+                  new Date('2022-2-13 03:24:00'),
+                  new Date('2022-2-14 03:24:00'),
+                ],
+                datasets: [
+                  {
+                    borderColor: '#d93730',
+                    backgroundColor: null,
+                    data: [0, 30, 200, 100, 280, 100, 400],
+                    fill: false,
+                    borderWidth: 2,
+                  },
+                ],
+              },
+            },
+            {
+              id: 4,
+              name: 'Jordan 4 Retro (2021)',
+              category: 'sneakers',
+              sku: 'CW6024-600',
+              colorway: 'Plum',
+              trend_24h: -10.5,
+              trend_7d: -10.5,
+              trend_30d: -12.5,
+              last_price: 23000,
+              last_7d_data: {
+                labels: [
+                  new Date('2022-2-8 03:24:00'),
+                  new Date('2022-2-9 03:24:00'),
+                  new Date('2022-2-10 03:24:00'),
+                  new Date('2022-2-11 03:24:00'),
+                  new Date('2022-2-12 03:24:00'),
+                  new Date('2022-2-13 03:24:00'),
+                  new Date('2022-2-14 03:24:00'),
+                ],
+                datasets: [
+                  {
+                    borderColor: '#d93730',
+                    backgroundColor: null,
+                    data: [0, 30, 200, 100, 280, 100, 400],
+                    fill: false,
+                    borderWidth: 2,
+                  },
+                ],
+              },
+            },
+            {
+              id: 5,
+              name: 'Jordan 4 Retro (2021)',
+              category: 'sneakers',
+              sku: 'CW6024-600',
+              colorway: 'Silver',
+              trend_24h: -10.5,
+              trend_7d: -10.5,
+              trend_30d: -12.5,
+              last_price: 23000,
+              last_7d_data: {
+                labels: [
+                  new Date('2022-2-8 03:24:00'),
+                  new Date('2022-2-9 03:24:00'),
+                  new Date('2022-2-10 03:24:00'),
+                  new Date('2022-2-11 03:24:00'),
+                  new Date('2022-2-12 03:24:00'),
+                  new Date('2022-2-13 03:24:00'),
+                  new Date('2022-2-14 03:24:00'),
+                ],
+                datasets: [
+                  {
+                    borderColor: '#d93730',
+                    backgroundColor: null,
+                    data: [0, 30, 200, 100, 280, 100, 400],
+                    fill: false,
+                    borderWidth: 2,
+                  },
+                ],
+              },
+            },
+          ]
+        },
+      },
+    },
+    data() {
+      return {
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [
+              {
+                type: 'time',
+                offset: false,
+                time: {
+                  unit: 'day',
+                },
+                gridLines: {
+                  display: false,
+                  drawBorder: false,
+                },
+                scaleLabel: {
+                  display: false,
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
+            yAxes: [
+              {
+                offset: false,
+                gridLines: {
+                  display: false,
+                  drawBorder: false,
+                },
+                scaleLabel: {
+                  display: false,
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
+          },
+          legend: {
+            display: false,
+          },
+          elements: {
+            point: {
+              radius: 0,
+            },
+          },
+        },
+      }
+    },
+  }
+  </script>
+  <style lang="sass" scoped>
+  @import '~/assets/css/_variables'
+  @import '~/assets/css/_typography'
+  
+  .trend-table-wrapper
+    width: calc( 100% - 16px )
+    margin: 0 auto
+    tr
+      border-top: 1px solid $color-gray-63
+    thead tr td
+      @include body-4-bold
+      color: $color-black-1
+      padding: 29px 3px
+      border: none
+    tbody
+      tr td
+        @include body-8-medium
+        vertical-align: middle
+        padding: 20px 3px
+        border: none
+  
+      .col-no
+        @include body-8-medium
+        color: $color-black-1
+  
+      .col-thumb
+        display: flex
+        justify-content: center
+        >div
+          width: 106px
+          height: 71px
+          overflow: hidden
+          display: flex
+          align-items: center
+          justify-content: center
+        @media (max-width: 550px)
+          padding: 4px 3px
+          >div
+            width: 80px
+            justify-content: start
+          .thumb-wrapper
+            width: 62px
+      .col-product-detail
+        .text-name
+          @include body-8-bold
+          color: $color-black-1
+        .text-color
+          @include body-10-normal
+          color: $color-gray-5
+          margin-top: 1px
+        @media (max-width: 550px)
+          .text-name
+            font-size: 11px
+            font-weight: $bold
+          .text-color
+            color: $color-gray-74
+            font-size: 11px
+            font-weight: $normal
+      .col-price
+        color: $color-black-1
+  
+      .col-trend-positive
+        color: $color-green-4
+  
+      .col-trend-negative
+        color: $color-red-5
+  
+      .col-graph
+        .trend-graph
+          width: 96px
+          height: 35px
+    @media (max-width: 550px)
+      .pricing
+        font-size: 11px
+        font-weight: $medium
+  </style>
+  
