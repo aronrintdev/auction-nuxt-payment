bf6b70931614ee06931ce73894972e75a28c7153
khawarjavaid <90414577+khawarjavaid@users.noreply.github.com>
2022-10-19 01:14:29 +0500

    Update ProductDetail.vue

--- components/Exchange/ProductDetail.vue
+++ components/Exchange/ProductDetail.vue
@@ -7,7 +7,7 @@
     >
       <Loader :loading="loading"></Loader>
     </div>
-    <div class="row">
+    <div v-if="!loading" class="row">
       <div class="col-lg-6 col-md-12 col-sm-12">
         <div class="row">
           <div class="col-lg-2">
@@ -105,7 +105,7 @@
           </div>
           <div class="col-lg-4"></div>
           <div class="row">
-            <div class="col-ld-12">
+            <div  id="tb-product" class="col-ld-12">
               <!-- ProducListCard Table -->
               <SimilarProductTable :products="products" />
             </div>
@@ -176,6 +176,7 @@ export default {
 
       ],
       sortBy: null,
+      showScroll: null,
       current: '24',
       // line chart data
       lineChartOptions: {
@@ -249,6 +250,11 @@ export default {
       this.current = category
     },
     // On filter by change.
+    handleSortBySelect(value) {
+      this.sortBy=value.value
+      this.loadPage()
+    },
+    // On filter by change.
     searchProduct(value) {
       this.search =  value
       this.loadPage()
@@ -256,6 +262,7 @@ export default {
     handleBuyClick() {},
     handleSellClick() {},
     loadPage() {
+      this.loading= true
       this.$axios
         .get('/products', {
           // .get('/stock-exchange', {
@@ -264,16 +271,19 @@ export default {
             page: this.currentPage,
             take: this.perPage,
             search: this.search,
+            sortBy: this.sortBy,
           },
         })
         .then((response) => {
           if (response.data) {
             console.log(response.data)
             this.products = response.data.data
+            this.loading= false
             // this.totalRows = response.data.total
           }
         })
         .catch((error) => {
+            this.loading= false
           this.$toasted.error(error.message)
         })
     },
