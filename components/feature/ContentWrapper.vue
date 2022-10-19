6222586536d8b8e29273db4fbd94cafd9cb4c643
Sameer <sameer.dev@deadstock.co>
2022-10-19 19:29:27 +0500

    crypto-payments page update

--- components/feature/ContentWrapper.vue
+++ components/feature/ContentWrapper.vue
@@ -4,6 +4,7 @@
       :title="title"
       :description="description"
       :backgroundImage="backgroundImage"
+      :backgroundImageSm="backgroundImageSm"
     />
 
     <div class="feature-content mx-auto">
@@ -48,6 +49,34 @@ export default {
     backgroundImage: {
       type: String,
       required: true,
+    },
+    backgroundImageSm: {
+      type: String,
+      required: true,
+    },
+    shopNowDescription: {
+      type: String,
+      required: true,
+    },
+    shopNowButtonText: {
+      type: String,
+      required: true,
+    },
+    previousPage: {
+      type: String,
+      default: '',
+    },
+    previousPageLink: {
+      type: String,
+      default: '',
+    },
+    nextPage: {
+      type: String,
+      default: '',
+    },
+    nextPageLink: {
+      type: String,
+      default: '',
     },
   },
 }
