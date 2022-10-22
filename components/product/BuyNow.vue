b8bc6f28ffb4121ae616dbe2b6206d53640f7591
aronrint <aronrint@gmail.com>
2022-10-22 13:40:34 +0300

    Buy now component
    - Updated the buy now component and added the wishlist functionality.

--- components/product/BuyNow.vue
+++ components/product/BuyNow.vue
@@ -14,16 +14,31 @@
           >{{ $t('products.buy_now') }}
           </Button>
           <Button
+            :id="`popover-wishlist-${product.id}`"
+            :tooltip-text="wishList ? wishList.name : ''"
+            tabindex="0"
             variant="light"
             class="px-4 border-0 border-radius-left-0"
+            @click="removeFromWishList"
           >
             <template #default>
-              <b-img width="18" :src="require('~/assets/img/product/heart-outline.svg')"></b-img>
+              <b-img v-if="!wishList" width="18" :src="require('~/assets/img/product/heart-outline.svg')"></b-img>
+              <b-img v-else width="18" :src="require('~/assets/img/icons/heart-red.svg')"></b-img>
             </template>
           </Button>
         </b-col>
       </b-row>
       <!-- Buy Now and Add to Cart and Add to Wishlist Button Group -->
+
+      <WishListPopover
+        v-if="!wishList"
+        :product="product"
+        :wish-list="wishList"
+        :target="`popover-wishlist-${product.id}`"
+        @wishlisted="onWishListed"
+        @show="wishListShow = true"
+        @hidden="wishListShow = false"
+      />
 
       <Button class="my-3" block variant="dark" @click="$emit('add-to-cart')">{{ $t('products.add_to_cart') }}</Button>
     </b-col>
@@ -31,18 +46,65 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import Button from '~/components/common/Button'
 import AlternativePaymentTitle from '~/components/product/AlternativePaymentTitle'
+import WishListPopover from '~/components/wish-list/Popover.vue'
 
 export default {
   name: 'BuyNowSection',
-  components: { Button, AlternativePaymentTitle},
+  components: { Button, AlternativePaymentTitle, WishListPopover },
   props: {
+    product: {
+      type: Object,
+      required: true,
+    },
     lowestPrice: {
       type: Number,
       required: true,
       default: 0.00
+    },
+  },
+  data() {
+    return {
+      wishListShow: false,
+      wishList:
+        this.product.wish_lists && this.product.wish_lists.length > 0
+          ? this.product.wish_lists[0]
+          : null
+    }
+  },
+  methods: {
+    ...mapActions({
+      removeProductsFromWishList: 'wish-list/removeProductsFromWishList',
+    }),
+    removeFromWishList() {
+      if (this.wishList) {
+        this.removeProductsFromWishList({
+          wishList: this.wishList,
+          ids: [this.product.id],
+        })
+        this.wishList = null
+        this.$emit('unwishlisted', this.product)
+      }
+    },
+    onWishListed(wishList) {
+      if (wishList) {
+        this.$set(this, 'wishList', wishList)
+        this.wishListShow = false
+        this.$emit('wishlisted', this.product, wishList)
+      }
     }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.border-radius-left-0
+  border-top-left-radius: 0
+  border-bottom-left-radius: 0
+
+.border-radius-right-0
+  border-top-right-radius: 0
+  border-bottom-right-radius: 0
+</style>
