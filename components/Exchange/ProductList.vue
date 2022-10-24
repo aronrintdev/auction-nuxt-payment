ad5eb0c78934f5fa4ff41639ce94198fe0165ad0
khawarjavaid <90414577+khawarjavaid@users.noreply.github.com>
2022-10-25 00:48:09 +0500

    add more btn added

--- components/Exchange/ProductList.vue
+++ components/Exchange/ProductList.vue
@@ -17,34 +17,53 @@
     </div>
 
     <!-- ProductTrendListCard Table -->
-      <div v-if="products.length > 0" class="mb-3">
-        <ProductTrendListCard :products="products" :activeHeaders="activeHeaders" :type="type" />
-        <Pagination
-          v-if="products.length > 0"
-          :value="currentPage"
-          :total="products.total"
-          :per-page="perPage"
-          :per-page-options="perPageOptions"
-          class="pagination-group"
-          @input="handlePageChange"
-          @page-click="handlePageClick"
-          @per-page-change="handlePerPageChange"
+    <div v-if="products.length > 0" class="mb-3">
+      <ProductTrendListCard
+        :products="products"
+        :activeHeaders="activeHeaders"
+        :type="type"
+      />
+      <Pagination
+        v-if="products.length > 0 && !isMobile"
+        :value="currentPage"
+        :total="products.length"
+        :per-page="perPage"
+        :per-page-options="perPageOptions"
+        class="pagination-group"
+        @input="handlePageChange"
+        @page-click="handlePageClick"
+        @per-page-change="handlePerPageChange"
+      />
+    </div>
+    <div v-else>
+      <div class="product-carousel-wrapper">
+        <div class="no-text py-5 text-center">No Products Found</div>
+      </div>
+    </div>
+    <div v-if="products.length > 0 && isMobile" class="ml-auto float-right">
+      <div class="view-more-wrapper d-flex align-items-end">
+        <span
+          variant="link"
+          class="btn-view-more font-primary text-gray-lightm mb-2 mr-3 d-lg-flex"
+          @click="loadMore"
+        >
+        View More
+        </span>
+        <img
+          class="pointer"
+          width="44px"
+          :src="require('assets/img/dashicons_arrow-right.png')"
+          @click="loadMore"
         />
       </div>
-      <div v-else>
-        <div class="product-carousel-wrapper">
-          <div class="no-text py-5 text-center">
-            No Products Found
-          </div>
-        </div>
-      </div>
+    </div>
   </div>
 </template>
 <script>
-import { Loader,Pagination } from '~/components/common'
+import { Loader, Pagination } from '~/components/common'
 import ExchangeFilter from '~/components/Exchange/Filters'
 import ProductTrendListCard from '~/components/Exchange/SimilarProductTable'
-import {PAGE_OPTIONS, PER_PAGE} from '~/static/constants/stock-exchange';
+import { PAGE_OPTIONS, PER_PAGE } from '~/static/constants/stock-exchange'
 
 export default {
   name: 'TopProductsList',
@@ -52,7 +71,7 @@ export default {
     Loader,
     ProductTrendListCard,
     ExchangeFilter,
-    Pagination
+    Pagination,
   },
   props: {
     loading: {
@@ -98,7 +117,7 @@ export default {
       return this.promotions.length ? this.promotions[0] : null
     },
     isMobile() {
-      if (this.screenWidth<= 760) {
+      if (this.screenWidth <= 760) {
         return true
       } else {
         return false
@@ -106,15 +125,14 @@ export default {
     },
   },
   created() {
-    this.loadPage();
+    this.loadPage()
     this.myEventHandler()
   },
   destroyed() {
-    window.removeEventListener('resize', this.myEventHandler);
-
+    window.removeEventListener('resize', this.myEventHandler)
   },
   mounted() {
-    window.addEventListener('resize', this.myEventHandler);
+    window.addEventListener('resize', this.myEventHandler)
   },
   methods: {
     // Search Data
@@ -127,36 +145,40 @@ export default {
       this.loadPage()
     },
 
+    loadMore() {
+      this.perPage += 10
+      this.loadPage()
+    },
     handlePageClick(bvEvent, page) {
       if (this.page !== page) {
-        this.perPage=page
+        this.perPage = page
         this.loadPage()
       }
     },
 
     handlePageChange(value) {
       if (this.page !== value) {
-        this.perPage=value
+        this.perPage = value
         this.loadPage()
       }
     },
 
     handlePerPageChange(value) {
       if (this.perPage !== value) {
-        this.perPage=value
+        this.perPage = value
         this.loadPage()
       }
     },
     // Get auctions list from API
     loadPage() {
       this.$axios
-        .get('/stock-exchange/'+this.$route.params.type, {
+        .get('/stock-exchange/' + this.$route.params.type, {
           params: {
             type: this.type,
             page: this.currentPage,
             take: this.perPage,
-            paginate:1,
-            ...this.filter
+            paginate: 1,
+            ...this.filter,
           },
         })
         .then((response) => {
