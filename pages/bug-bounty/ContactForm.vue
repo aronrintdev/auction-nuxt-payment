05dd22a8b0240105198847d989528d32771ef325
Usman Ali Sarwar <usmanalisarwar.az@gmail.com>
2022-10-27 23:32:21 +0500

    post request integrated

--- pages/bug-bounty/ContactForm.vue
+++ pages/bug-bounty/ContactForm.vue
@@ -220,7 +220,7 @@
               <img class="attachment cursor-pointer" src="~/assets/img/attachment.png" @click="showModal">
               <p class="ml-2 text-black fs-14 fw-5">{{ $t('bounty.form.attachment.title') }}</p>
             </div>
-            <UploadModal :form="fileForm" :show="showDocModal" @closed="modalClosed" :requirement="fileRequirement" @uploaded="fileUploaded"/>
+            <UploadModal v-model="form.images" :form="fileForm" :show="showDocModal" @closed="modalClosed" :requirement="fileRequirement" @uploaded="fileUploaded"/>
 
             <div class="d-flex">
               <ValidationProvider
