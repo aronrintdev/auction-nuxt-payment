125e5f00639c7587c34bca0c0186f46ee9258537
Mahabubul Hasan <hasan.dev@deadstock.co>
2022-10-27 12:39:07 +0600

    Fix style after resolving conflict

--- pages/profile/support/contact-us/index.vue
+++ pages/profile/support/contact-us/index.vue
@@ -103,7 +103,7 @@ export default {
 
   .box, .form-box
     background: $color-white-14
-    border: 1px solid $color-white-21
+    border: 1px solid $color-white-25
     border-radius: 10px
 
 @media (max-width: 767px)
