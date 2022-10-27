3aed9a397199afcc104173441c5596816d58c24b
aronrint <aronrint@gmail.com>
2022-10-27 22:08:36 +0300

    Checkout selling shopping bag title
    - Added the Checkout selling shopping bag title in order to be reused for all the section titles.

---
+++ components/checkout/selling/mobile/ShoppingBagTitle.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Shopping Bag Title -->
+  <b-row class="title-wrapper">
+    <b-col sm="12" md="12" :class="{ 'text-center': textCenter }" class="d-flex align-items-center justify-content-center">
+      <ArrowLeftGray17SVG v-if="backLink" class="arrow-left" @click="$emit('click')" />
+      <span class="body-12-bold">{{ title }}&nbsp;</span>
+      <span class="body-12-normal">{{ additionalTitle }}</span>
+    </b-col>
+  </b-row>
+  <!-- End of Shopping Bag Title -->
+</template>
+
+<script>
+import LocaleMessage from 'vue-i18n'
+import ArrowLeftGray17SVG from '~/assets/img/shopping-cart/arrow-left-gray-17.svg?inline'
+import shoppingCartMixin from '~/plugins/mixins/shopping-cart'
+
+export default {
+  name: 'ShoppingBagTitle',
+  components: { ArrowLeftGray17SVG },
+  mixins: [shoppingCartMixin],
+  props: {
+    title: {
+      type: [String, LocaleMessage],
+      required: true,
+      default: '',
+    },
+    additionalTitle: {
+      type: [String, LocaleMessage],
+      required: false,
+      default: '',
+    },
+    backLink: {
+      type: Boolean,
+      default: false,
+    },
+    textCenter: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.title-wrapper
+  margin: 8px 18px 0
+
+.arrow-left
+  position: absolute
+  left: 0
+</style>
