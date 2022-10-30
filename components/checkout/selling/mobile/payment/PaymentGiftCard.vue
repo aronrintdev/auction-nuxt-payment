802181f5a91ff5a5b2bf003fa7b7f4b81a0b76e5
aronrint <aronrint@gmail.com>
2022-10-30 18:59:34 +0200

    Checkout selling payment giftcard component
    - Added the Checkout selling payment giftcard component to be rendered when the user adds a new gift card.

---
+++ components/checkout/selling/mobile/payment/PaymentGiftCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-row>
+    <b-col cols="12" sm="12" class="px-0">
+      <!-- Top Title -->
+      <ShoppingBagTitle
+        :title="$t('shopping_cart.add_gift_card')"
+        text-center
+        class="mt-0"
+      >
+        <template #back-arrow>
+          <ArrowLeftBlackSVG class="back-arrow" @click="emitRenderComponentEvent($parent.$options.components.PaymentOptionsMenu.name)"/>
+        </template>
+      </ShoppingBagTitle>
+      <!-- End of Top Title -->
+
+      <GiftCardDetailsForm v-if="! giftCard" @gift-card-found="handleGiftCardFound" />
+
+      <GiftCardAmountForm v-else :selected-gift-card="giftCard" @gift-card-applied="handleGiftCardApplied" />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import emitEventMixin from '~/plugins/mixins/emit-event'
+import ShoppingBagTitle from '~/components/checkout/selling/mobile/ShoppingBagTitle'
+import ArrowLeftBlackSVG from '~/assets/img/shopping-cart/arrow-left-black.svg?inline'
+import GiftCardDetailsForm from '~/components/checkout/selling/mobile/payment/GiftCardDetailsForm'
+import GiftCardAmountForm from '~/components/checkout/selling/mobile/payment/GiftCardAmountForm'
+
+export default {
+  name: 'PaymentGiftCard',
+  components: { GiftCardAmountForm, ShoppingBagTitle, ArrowLeftBlackSVG, GiftCardDetailsForm },
+  mixins: [ emitEventMixin ],
+  data() {
+    return {
+      giftCard: false
+    }
+  },
+  methods: {
+    ...mapActions({
+      addGiftCard: 'order-details/addGiftCard'
+    }),
+    handleGiftCardFound(giftCard) {
+      this.giftCard = giftCard
+    },
+    handleGiftCardApplied(giftCard) {
+      this.addGiftCard({
+        giftCard,
+      }).then(() => {
+        this.$toasted.success(this.$t('shopping_cart.gift_card_applied').toString())
+        this.emitRenderComponentEvent(this.$parent.$options.components.OrderSummary.name)
+      })
+    }
+  }
+}
+</script>
