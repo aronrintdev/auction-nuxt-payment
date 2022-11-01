25f80b0be12f9ebcb98dc68255fdea7037af031e
atif-deadstock <97760384+atif-deadstock@users.noreply.github.com>
2022-11-01 10:18:48 -0600

    loadmore

--- pages/shop/products.vue
+++ pages/shop/products.vue
@@ -1,61 +1,100 @@
 <template>
-  <div>
-    <p v-if="$fetchState.pending">Fetching planets....</p>
-    <p v-else-if="$fetchState.error">Error while fetching planets</p>
-    <ul v-else>
-      <li v-for="planet in planets" :key="planet.slug">
-        <NuxtLink :to="planet.slug">
-          {{ planet.title }}
-        </NuxtLink>
-      </li>
-    </ul>
-  </div>
+  <article>
+    <section v-for="(n, index) in pageOffset" :key="index">
+      <img :src="`https://picsum.photos/500/400?image=` + index" alt="">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi est repellat. Ex doloribus molestias hic
+        dolorem provident. Incidunt numquam culpa ut alias eveniet at quasi, reiciendis suscipit qui. Iure.
+      </p>
+    </section>
+    <div class="below">
+      <div id="scroll-trigger" ref="infinitescrolltrigger"></div>
+      <div v-if="showloader" class="circle-loader"></div>
+    </div>
+  </article>
 </template>
 <script>
+import Vue from 'vue';
+import VueLoadmore from 'vuejs-loadmore';
 
+Vue.use(VueLoadmore);
 export default {
   layout: 'IndexLayout',
   fetchOnServer: false,
-  async fetch() {
-    this.planets = await fetch('https://api.nuxtjs.dev/planets').then((res) =>
-      res.json()
-    )
-  },
   data() {
     return {
-      planets: [],
+      currentPage: 1,
+      maxPerPage: 2,
+      totalResults: 100,
+      showloader: true
+    };
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.totalResults / this.maxPerPage)
+    },
+    pageOffset() {
+      return this.maxPerPage * this.currentPage
+    },
+  },
+  mounted() {
+    this.scrollTrigger();
+  },
+  methods: {
+    scrollTrigger() {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > 0 && this.currentPage < this.pageCount) {
+            this.showloader = true;
+            setTimeout(() => {
+              this.currentPage += 1;
+              this.showloader = false;
+            }, 1000) // ajax call here;
+          }
+        });
+      });
+      observer.observe(this.$refs.infinitescrolltrigger);
     }
   },
 }
 </script>
 <style lang="sass" scoped>
 @import '~/assets/css/_variables'
-.container-shop
-  @media (min-width: 1024px)
-    .scroll-to-top
-      display: flex !important
-      bottom: -100px
-      right: 60px
-      border: 2px solid $color-blue-20
-      width: 40px
-      aspect-ratio: 1
-      z-index: 1020
-      transition: 0.5s all ease-in-out
-      &.scrolled
-        bottom: 40px
-  .no-items-found
-    img
-      width: 130px
-    &-title
-      @include heading-7
-      color: $color-gray-5
-      font-family: $font-sp-pro
-    &-subtitle
-      @include body-1-regular
-      font-family: $font-sp-pro
-      color: $black
-::v-deep .section-header
-  @media (max-width: 576px)
-    margin: 30px 12.5px
+.article
+  margin: 0 auto
+  width: 400px
+  .section
+    width: 400px
+    margin-bottom: 20px
+    border-radius: 10px
+    background-color: #efefef
+    color: #04525A
+    overflow: hidden
+    .p
+      margin: 0
+      padding: 10px 20px 
+.below
+  position: relative
+  width: 400px
+  height: 100px
+
+  #scroll-trigger
+    height: 50px
+  .circle-loader
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    width: 50px
+    height: 50px
+    border-radius: 50%
+    border: 5px solid rgba(255, 255, 255, .2)
+    border-top: 5px solid #fff
+    animation: animate 1.5s infinite linear
+@keyframes animate
+  0%
+    transform: translate(-50%, -50%) rotate(0deg)
+  100%
+    transform: translate(-50%, -50%) rotate(360deg)
 </style>
   
