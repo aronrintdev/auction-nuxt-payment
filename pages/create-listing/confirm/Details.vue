200e2a3193ccc43d947369994890a0a5ccda307a
Jeremy Fall <jeremy.dev@deadstock.co>
2022-11-01 02:44:49 -0700

    add reserve info tooltip

--- pages/create-listing/confirm/Details.vue
+++ pages/create-listing/confirm/Details.vue
@@ -75,7 +75,7 @@
             <img
               v-b-tooltip.hover
               :src="infoIcon"
-              :title="$tc('create_listing.confirm.reserve_tooltip_info')"
+              :title="$tc('create_listing.confirm.reserve_info_short')"
               class="icon-info position-absolute mt-n2 mr-n5 scale-2"
             />
           </span>
