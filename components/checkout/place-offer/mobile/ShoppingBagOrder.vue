2bef0a0d8806a3b3a99e685380d1afeb2bb3f554
aronrint <aronrint@gmail.com>
2022-11-02 20:01:52 +0200

    Place offer shopping bag order component
    - Added the Place offer shopping bag order component in order to conditionally render components in within.

---
+++ components/checkout/place-offer/mobile/ShoppingBagOrder.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Dynamic Component Rendering of OrderSummary, BillingForm, ShippingForm, PaymentOption, PaymentCard, PaymentOptionError & PaymentGiftCard -->
+  <component :is="renderedComponent"></component
+  ><!-- End of Dynamic Component Rendering -->
+</template>
+
+<script>
+import OfferSummary from '~/components/checkout/place-offer/mobile/OfferSummary'
+import ListItemOptionsMenu from '~/components/checkout/place-offer/mobile/ListItemOptionsMenu'
+
+export default {
+  name: 'ShoppingBagOrder',
+  components: {
+    OfferSummary,
+    ListItemOptionsMenu
+  },
+  data: () => {
+    return {
+      renderedComponent: OfferSummary.name,
+    }
+  },
+  created() {
+    this.$nuxt.$on('renderComponent', (componentName) => {
+      this.renderedComponent = componentName
+    })
+  },
+}
+</script>
