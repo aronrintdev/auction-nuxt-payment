8f414048283226b3fee2a8cb6c0d610d3ab4e306
aronrint <aronrint@gmail.com>
2022-11-03 22:29:56 +0200

    Place offer shopping bag component
    - Updated the Place offer shopping bag component and replaced all calculations with the new offer details mixin.

--- components/checkout/place-offer/mobile/ShoppingBag.vue
+++ components/checkout/place-offer/mobile/ShoppingBag.vue
@@ -79,20 +79,21 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 import emitEventMixin from '~/plugins/mixins/emit-event'
+import offerDetailsMixin from '~/plugins/mixins/offer-details'
 import ShoppingBagTitle from '~/components/checkout/selling/mobile/ShoppingBagTitle'
 import ListItem from '~/components/checkout/place-offer/mobile/ListItem'
 import PromoCodeInput from '~/components/checkout/common/PromoCodeInput'
 import OrderSummaryCard from '~/components/checkout/common/OrderSummaryCard'
 import Button from '~/components/common/Button'
 import ShoppingBagOrder from '~/components/checkout/place-offer/mobile/ShoppingBagOrder'
-import { AMOUNT_OFFSET, BAD_REQUEST, FIXED_PRODUCT, NOT_FOUND, PERCENT, PERCENT_OFFSET } from '~/static/constants'
+import { BAD_REQUEST, NOT_FOUND } from '~/static/constants'
 
 export default {
   name: 'ShoppingBag',
   components: { ShoppingBagTitle, ListItem, PromoCodeInput, OrderSummaryCard, Button, ShoppingBagOrder },
-  mixins: [ emitEventMixin ],
+  mixins: [ emitEventMixin, offerDetailsMixin ],
   data() {
     return {
       offerDuration: 10, // TODO: NP - Fill this once implemented.
@@ -100,79 +101,8 @@ export default {
       bottomSheetMaxHeight: '95%',
     }
   },
-  computed: {
-    ...mapGetters({
-      offerDetails: 'offer/getOfferDetails',
-      shippingFee: 'order-settings/getShippingFee',
-      processingFee: 'order-settings/getProcessingFee',
-      taxRate: 'tax-rate/getTaxRate',
-      promoCode: 'order-details/getPromoCode',
-      billingAddress: 'auth/getBillingAddress',
-    }),
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getSubTotal: (vm) => {
-      return vm.offerDetails.bid_price
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getItems: (vm) => {
-      const items = []
-      items.push({ key: 'sub_total', label: vm.$t('place_offer.offer_amount'), value: vm.getSubTotal > 0 ? vm.getSubTotal : 0 })
-      items.push({ key: 'sub_total_after_discount', label: '', value: vm.getSubtotalAfterDiscount > 0 ? vm.getSubtotalAfterDiscount : 0 })
-      items.push({ key: 'shipping_fee', label: vm.$t('shopping_cart.shipping_fee'), value: vm.shippingFee > 0 ? vm.shippingFee : 0 })
-      items.push({ key: 'processing_fee', label: vm.$t('shopping_cart.processing_fee'), value: vm.getProcessingFee > 0 ? vm.getProcessingFee : 0 })
-      items.push({ key: 'tax', label: vm.$t('shopping_cart.tax'), value: vm.getTax > 0 ? vm.getTax : 0 })
-
-      return items
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getSubtotalAfterDiscount: (vm) => {
-      return Math.max(vm.getSubTotal - vm.getPromoDiscount, 0)
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getProcessingFee: (vm) => {
-      return Math.trunc(vm.processingFee * vm.getSubTotal)
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getTax: (vm) => {
-      return Math.trunc(vm.taxRate * vm.getSubTotal)
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getTotal: (vm) => {
-      return vm.getSubtotalAfterDiscount + vm.shippingFee + vm.getProcessingFee + vm.getTax
-    },
-    // Expects a View Model. Use the variable vm (short for ViewModel) to refer to our Vue instance.
-    getPromoDiscount: (vm) => {
-      let discount = 0
-
-      if (vm.promoCode.code) {
-        switch (vm.promoCode.type) {
-          case FIXED_PRODUCT: {
-
-            if (vm.offerDetails.product.sku === vm.promoCode.sku) {
-              discount += vm.promoCode.amount * AMOUNT_OFFSET
-            }
-
-            break;
-          }
-          case PERCENT: {
-            discount += vm.getSubTotal * (vm.promoCode.amount / PERCENT_OFFSET)
-
-            break;
-          }
-          default:
-            discount += vm.promoCode.amount * AMOUNT_OFFSET
-        }
-      }
-
-      return discount
-    },
-  },
-  mounted() {
-    this.getTaxRateByZip({ zip: this.billingAddress.zipCode })
-  },
   methods: {
     ...mapActions({
-      getTaxRateByZip: 'tax-rate/getTaxRateByZip',
       addPromoCode: 'order-details/addPromoCode',
       removePromoCode: 'order-details/removePromoCode',
     }),
@@ -200,7 +130,7 @@ export default {
       this.$refs.shoppingBagOffer.open()
     },
     handleNextButtonClick() {
-      this.emitRenderComponentEvent(this.$options.components.ShoppingBagOrder.components.OfferSummary.name)
+      this.emitRenderComponentEvent(this.$options.components.ShoppingBagOrder.components.CheckoutSummary.name)
       this.openBottomSheet('95%')
     },
     handleItemOptionsClick() {
