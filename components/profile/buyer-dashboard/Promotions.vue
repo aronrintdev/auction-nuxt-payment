6972b91302f0da8772c647def4aa3febbaf51cf1
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2022-11-04 20:42:19 +0400

    Buyer promotion fixes

--- components/profile/buyer-dashboard/Promotions.vue
+++ components/profile/buyer-dashboard/Promotions.vue
@@ -12,36 +12,36 @@
       >
     </div>
     <div class="container">
-      <div class="row mt-4 my-sm-4">
+      <div v-if="promotion && !loading" class="row mt-4 my-sm-4">
         <div
-          class="col-md-6 d-flex align-items-center align-items-sm-baseline flex-sm-column justify-content-center my-4 my-sm-0 order-2 order-sm-1"
+            class="col-md-6 d-flex align-items-center align-items-sm-baseline flex-sm-column justify-content-center my-4 my-sm-0 order-2 order-sm-1"
         >
           <h2
               class="fs-30 fw-7 font-primary mb-0 text-left promotion-title mr-2 mr-sm-0 text-nowrap text-truncate mr-1"
           >
-            {{ promotions.name }}
+            {{ promotion.name }}
           </h2>
           <div
-            class="divider bg-dark w-25 my-4 mx-auto mx-sm-0 d-none d-sm-block"
+              class="divider bg-dark w-25 my-4 mx-auto mx-sm-0 d-none d-sm-block"
           ></div>
-          <div class="d-none d-sm-block">
-            <CountdownTimer :countDownToTime="countDownToTime" />
+          <div v-if="countDownToTime" class="d-none d-sm-block">
+            <CountdownTimer :countDownToTime="countDownToTime"/>
           </div>
           <div
-            class="your-entries d-flex gap-2 align-items-center justify-content-center justify-content-sm-start"
+              class="your-entries d-flex gap-2 align-items-center justify-content-center justify-content-sm-start"
           >
             <img src="~/assets/img/icons/profile/entries.svg" />
             <h4 class="fs-18 fw-7 font-primary mb-0 mt-1 description">
               {{ $t('buyer_dashboard.promotions.your_entries') }}
             </h4>
             <h4 class="text-orange fs-18 fw-7 font-primary mb-0 mt-1">
-              {{ promotions.entries_count }}
+              {{ promotion.entries_count }}
             </h4>
           </div>
         </div>
         <div class="col-md-6 d-flex justify-content-center order-1 order-sm-2">
           <div class="car-main justify-content-center d-flex">
-            <img :src="promotions.promotion_image" class="img-fluid" />
+            <img :src="promotion.promotion_image" class="img-fluid"/>
           </div>
         </div>
         <div class="d-sm-none d-inline-block order-3 w-100 text-center ">
@@ -58,20 +58,30 @@
           >
         </div>
       </div>
+      <div v-if="!loading && !promotion" class="d-flex align-items-center justify-content-center my-4">
+        <div class="fs-20 fw-7 font-primary ">
+          {{ $t('buyer_dashboard.promotions.no_active') }}
+        </div>
+      </div>
+      <div v-if="loading" class="d-flex align-items-center justify-content-center my-4">
+        <Loader :loading="loading"></Loader>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import CountdownTimer from './CountdownTimer'
 import screenSize from '~/plugins/mixins/screenSize';
+import Loader from '~/components/common/Loader';
 export default {
   name: 'Promotions',
-  components: {CountdownTimer},
+  components: {Loader, CountdownTimer},
   mixins: [screenSize],
   data() {
     return {
-      promotions: [],
-      countDownToTime: '',
+      promotion: null,
+      countDownToTime: null,
+      loading: false
     }
   },
   mounted() {
@@ -79,17 +89,20 @@ export default {
   },
   methods: {
     getPromotion() {
+      this.loading = true
       this.$axios
-        .get('/dashboard/buyer/promotions')
-        .then((res) => {
-          this.promotions = res.data.data[0]
-          this.countDownToTime = new Date(
-            Date.parse(this.promotions.end_at + ' 00:00:00')
-          )
-        })
-        .catch((err) => {
-          this.logger.logToServer(err.response)
-        })
+          .get('/dashboard/buyer/promotions')
+          .then((res) => {
+            this.promotion = res.data.data
+            if (this.promotion) {
+              this.countDownToTime = this.$moment(this.promotion.end_at).endOf('day').valueOf()
+            }
+          })
+          .catch((err) => {
+            this.logger.logToServer(err.response)
+          }).finally(() => {
+        this.loading = false
+      })
     },
   },
 }
