ae1a6e03be8c4aa9501274f755e06162dcb3a8cc
aronrint <aronrint@gmail.com>
2022-11-09 19:15:02 +0200

    Sell now index page
    - Updated Sell now index page and added an event in order to toggle the visibility of the header.

--- pages/checkout/sell-now/index.vue
+++ pages/checkout/sell-now/index.vue
@@ -31,6 +31,7 @@ export default {
     }
   },
   beforeMount() {
+    this.$root.$emit('hide-header', { hideHeader: true })
     enquireScreenSizeHandler((type) => {
       this.$store.commit('size/setScreenType', type)
     })
@@ -40,11 +41,14 @@ export default {
     this.getOrderSettings()
     this.addVendorPayoutMethod()
     // If the user is a vendor
-    if(this.isVendor){
+    if (this.isVendor){
       this.getVendorCommission(this.$store.state.auth.user.vendor.id)
     }
 
     this.getGlobalCommission()
+  },
+  beforeDestroy() {
+    this.$root.$emit('hide-header', { hideHeader: false })
   },
   methods: {
     ...mapActions({
