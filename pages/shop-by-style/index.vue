718416e92a3ee5d3620eecc33cafaf593090eed9
Atif Rehman <atif.dev@deadstock.co>
2022-11-09 18:22:05 -0600

    fix images of shop by style

--- pages/shop-by-style/index.vue
+++ pages/shop-by-style/index.vue
@@ -126,6 +126,7 @@ export default {
         .get('shop-by-style', {
           params: {
             selectedType: this.type,
+            pageName: 'BROWSE',
             filters
           },
         })
