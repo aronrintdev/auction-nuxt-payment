1d59e58c187f62fab4f725af0dddcd9cae8e46f6
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2022-11-11 23:30:41 +0400

    Fix support UI mobile

--- components/profile/support/Searchbar.vue
+++ components/profile/support/Searchbar.vue
@@ -2,25 +2,38 @@
   <div class="support-search-box" :class="{ 'mx-auto': centered }">
     <div class="border d-flex align-items-center br-8 bg-white p-md-2">
       <img
-        :src="require('~/assets/img/icons/search.svg')"
-        class="search-icon fs-20"
+          :src="require('~/assets/img/profile/mobile/mobile-search.svg')"
+          class="search-icon fs-20"
       />
       <div class="w-100">
         <input type="search" :placeholder="$t('preferences.profile.support.searchPlaceholer')"
-               class="form-control no-border bg-white br-8"/>
+               :class="mobileClass" class="form-control no-border bg-white br-8"/>
       </div>
     </div>
   </div>
 </template>
 <script>
+import screenSize from '~/plugins/mixins/screenSize';
+
 export default {
+  mixins: [screenSize],
   props: {
     centered: Boolean,
-  },
+  }
 }
 </script>
 <style lang="sass">
 @import '~/assets/css/_variables'
+.form-control
+  &.mobile
+    &::placeholder
+      @include body-9-regular
+      font-family: $font-montserrat
+      font-style: normal
+      letter-spacing: 0.06em
+      color: $color-gray-5
+
+
 .support-search-box
   width: 700px
   font-family: $font-montserrat
--- pages/profile/support/index.vue
+++ pages/profile/support/index.vue
@@ -1,28 +1,53 @@
 <template>
   <div class="support-page bg-white h-100">
-    <section class="support-hero bg-lightgrey text-center p-md-5 p-3">
+    <div :class="mobileClass" class="support-hero bg-lightgrey text-center p-md-5 p-3">
       <h1 class="fs-52 fw-6 font-primary d-none d-md-block">
         {{ $t('preferences.profile.support.title') }}
       </h1>
       <h2 class="fs-32 fw-6 text-base-blue font-primary">
         {{ $t('preferences.profile.support.sub_heading') }}
       </h2>
-      <Searchbar class="mt-md-5 mt-3" centered />
-    </section>
-    <NavigationLinks class="mt-5" centered />
+      <Searchbar :class="{
+        'mt-md-5 mt-3': !isScreenXS,
+        'mb-7': isScreenXS
+      }" centered/>
+    </div>
+    <NavigationLinks :class="{
+        'mt-5': !isScreenXS,
+        'mt-42': isScreenXS
+      }" centered/>
   </div>
 </template>
 <script>
 import NavigationLinks from '~/components/profile/support/NavigationLinks'
 import Searchbar from '~/components/profile/support/Searchbar'
+import screenSize from '~/plugins/mixins/screenSize';
 export default {
   name: 'Support',
-  components: { NavigationLinks, Searchbar },
+  components: {NavigationLinks, Searchbar},
+  mixins: [screenSize],
   layout: 'Profile',
 }
 </script>
 <style scoped lang="sass">
 @import '~/assets/css/_variables'
+.mb-7
+  margin-bottom: 7px
+
+.mt-42
+  margin-top: 42px
+
+.support-hero
+  &.mobile
+    background-color: $color-white-4
+    padding: 23px 13px !important
+
+    h2
+      @include body-4-medium
+      font-family: $font-montserrat
+      font-style: normal
+      color: $color-blue-20
+      margin-bottom: 24px
 
 @media (max-width: 767px)
   .support-page
