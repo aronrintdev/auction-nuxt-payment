543038e6cddccddb51ee758ef4c97c4c5ef5cd21
Babar Saleh Hayat <babar.dev@deadstock.co>
2022-11-12 18:23:25 +0500

    trade lsiting

--- pages/profile/trades/dashboard/TradeListingItemsWeb.vue
+++ pages/profile/trades/dashboard/TradeListingItemsWeb.vue
@@ -13,29 +13,34 @@
         </b-checkbox>
       </b-row>
       <div class="d-flex justify-content-between p-4" @click="showTrade(trade.id)">
-        <div class="d-block">
-          <div class="trade-id">{{ $t('trades.trade_id') }} #{{ trade.id }}</div>
-          <div class="listed-time">{{ $t('trades.listed_on') }} {{
-              trade.created_at | formatDateTimeString
-            }}
+        <div class="d-flex">
+          <div>
+            <div class="trade-id">{{ $t('trades.trade_id') }} #{{ trade.id }}</div>
+            <div class="listed-time">{{ $t('trades.listed_on') }} {{
+                trade.created_at | formatDateTimeString
+              }}
+            </div>
           </div>
-        </div>
-        <div class="new-offers pr-3">
-          <ul>
-            <li>{{ $t('trades.new_offers', {'0': trade.new_offers}) }}</li>
-          </ul>
+         <div>
+           <div class="new-offers ml-4"> {{ $t('trades.new_offers', {'0': trade.new_offers}) }}</div>
+           <div class="view-detail-text ml-4"> View Details</div>
+         </div>
         </div>
       </div>
-      <div class="inner-items-listed ml-auto mr-auto" @click="showTrade(trade.id)">
-        <div class="inner-heading-listing text-center p-2">
-          {{ $t('trades.your_listed_items') }}
-        </div>
-        <div v-for="(offer) in trade.offers" :key="'trade-offer-'+offer.id"
-             class="d-flex align-items-center ml-4">
-          <img :src="offer.inventory.product | getProductImageUrl" class="inner-item-image h-auto">
-          <ul class="inner-item-text">
-            <li class="pt-3 text-truncate product-name">{{ offer.inventory.product.name }}</li>
-          </ul>
+      <div class="d-flex justify-content-center align-content-center"  @click="showTrade(trade.id)">
+        <div class="d-flex justify-content-center align-content-center" v-for="(offer) in trade.offers" :key="'trade-offer-'+offer.id">
+          <div class="d-inline body-section-box m-1">
+            <div class="d-flex justify-content-center align-content-center">
+              <img class="image-tarde pt-4" :src="offer.inventory.product | getProductImageUrl">
+            </div>
+
+            <div class="bottom-section mt-4">
+              <div class="product-name pt-1">{{ offer.inventory.product.name }}</div>
+              <div class="product-size "><span>Size : </span>{{offer.inventory.size.id }}</div>
+              <div class="product-size "><span>Box : </span>{{offer.inventory.packaging_condition.name}}</div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -85,7 +90,8 @@ export default {
   font-family: $font-family-sf-pro-display
   font-style: normal
   @include body-12-bold
-  color: $color-blue-1
+  color: #667799
+  font-weight: $bold
 
 .new-offers
   font-family: $font-family-sf-pro-display
@@ -125,4 +131,47 @@ export default {
 
 .product-name
   width: 290px
+.view-detail-text
+  font-family: $font-family-sf-pro-display
+  font-style: $normal
+  font-weight: 500
+  @include body-13
+  line-height: 19px
+  text-decoration-line: underline
+  color: $color-gray-5
+.inner-item-image
+  width: 58px
+
+.inner-item-text
+  font-family: $font-family-sf-pro-display
+  font-style: normal
+  @include body-12-normal
+  color: $color-gray-5
+
+.product-name
+  width: 90px
+  font-family: $font-family-sf-pro-display
+  font-style: normal
+  @include body-6-medium
+  white-space: nowrap
+  overflow: hidden
+  text-overflow: ellipsis
+.product-size
+  font-family: $font-family-sf-pro-display
+  font-style: normal
+  font-weight: $normal
+  font-size: 10px
+.body-section-box
+  height: 215px
+  width: 140px
+  border-radius: 0px
+  background: $color-white-4
+.bottom-section
+  height: 67px
+  //width: 213px
+  background: $color-white-1
+.image-tarde
+  width: 125px
+.box-pro
+  background: $color-white-4
 </style>
