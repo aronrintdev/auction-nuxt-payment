6c0191ed1bb2147cf07d6d1fccf8bae6841ed4d5
Babar Saleh Hayat <babar.dev@deadstock.co>
2022-11-16 22:20:45 +0500

    fixes

--- pages/profile/trades/dashboard/_id/index.vue
+++ pages/profile/trades/dashboard/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="isResponsive">
+    <div v-if="isResponsive" class="responsive-div">
       <b-row v-if="offer !== null">
         <b-col :md="isPayment ? 9 : 12">
           <div class="">
@@ -9,18 +9,18 @@
                 <div class="item-head-trade-hub-mobile">{{ $t('trades.trade_arena.theirs') }}</div>
                 <div v-for="(item) in lastSubmittedOffer.theirs_items"
                      :key="item.id" class="mb-4 ml-1 mt-2">
-                  <div class="p-2 image-small-size">
+                  <div class="p-2 image-small-size position-relative">
                     <img class="item-image-small" :src="item.inventory.product | getProductImageUrl"
                          :class="{'item-image-cond-small'
                        :(lastSubmittedOffer.theirs_items.length > ONE_ITEM || lastSubmittedOffer.yours_items.length) }"/>
+                    <div class="overlay-mob"></div>
                   </div>
 
                   <div class="item-caption-small">
                     <span class="item-name-small">{{ item.inventory.product.name }}</span>
                     <span
-                      class="item-box-condition-small">Box: {{ item.inventory.packaging_condition.name }}</span>
-                    <span class="item-caption-description">{{ item.inventory.product.colorway }}</span>
-                    <span class="item-size">Size: {{ item.inventory.size.size }}</span>
+                      class="item-box-condition-small">{{$tc('common.box')}}: {{ item.inventory.packaging_condition.name }}</span>
+                    <span class="item-caption-description">{{ item.inventory.product.colorway }},{{$tc('common.size')}}{{ item.inventory.size.size }}</span>
                   </div>
                 </div>
               </div>
@@ -40,16 +40,16 @@
                 <div v-if="lastSubmittedOffer.yours_items.length" >
                   <div v-for="(item) in lastSubmittedOffer.yours_items"
                    :key="item.id" class="mb-4 mt-2">
-                    <div class="p-2 image-small-size">
+                    <div class="p-2 image-small-size position-relative">
                       <img class="item-image-small" :src="item.inventory.product | getProductImageUrl" alt="image"
                            :class="{'item-image-cond-small':(lastSubmittedOffer.theirs_items.length > ONE_ITEM || lastSubmittedOffer.yours_items.length) }" />
+                      <div class="overlay-mob"></div>
                     </div>
                     <div class="item-caption-small">
                       <span class="item-name-small">{{ item.inventory.product.name }}</span>
                       <span
-                        class="item-box-condition-small">Box : {{ item.inventory.packaging_condition.name }}</span>
-                      <span class="item-caption-description-small">{{ item.inventory.product.colorway }}</span>
-                      <span class="item-size">Size : {{ item.inventory.size.size }}</span>
+                        class="item-box-condition-small">{{$tc('common.box')}}: {{ item.inventory.packaging_condition.name }}</span>
+                      <span class="item-caption-description-small">{{ item.inventory.product.colorway }},{{$tc('common.size')}}{{ item.inventory.size.size }}</span>
                     </div>
                   </div>
                 </div>
@@ -106,13 +106,13 @@
               <div class="d-flex align-items-center justify-content-between">
                 <div class="offer-id-sm">{{ $t('trades.offer_id') }} #{{lastSubmittedOffer.id}}</div>
                 <div class="offer-type px-3">
-                  <img 
-                    v-if="lastSubmittedOffer.offer_type === 'sent'" 
-                    :src="require('~/assets/img/trades/SentType.svg')" 
+                  <img
+                    v-if="lastSubmittedOffer.offer_type === 'sent'"
+                    :src="require('~/assets/img/trades/SentType.svg')"
                   >
-                  <img 
-                    v-else-if="lastSubmittedOffer.offer_type === 'received'" 
-                    :src="require('~/assets/img/trades/ReceivedType.svg')" 
+                  <img
+                    v-else-if="lastSubmittedOffer.offer_type === 'received'"
+                    :src="require('~/assets/img/trades/ReceivedType.svg')"
                   >
                   {{ $t(lastSubmittedOffer.offer_type_translation )}}
                 </div>
@@ -123,12 +123,12 @@
           </div>
           <div class="current-trade">{{ $t('common.current_trade') }}</div>
           <div class="offer-card">
-            <div 
+            <div
               class="d-flex flex-column px-3 px-lg-0"
             >
               <div class="d-flex justify-content-between col-12 col-md-8 mb-2 mx-auto">
                 <div class="value">
-                  {{ $t('common.their_value') }} 
+                  {{ $t('common.their_value') }}
                   <span class="ml-1 price">{{ getTheirTotal() }}</span>
                 </div>
                 <div class="value">
@@ -136,11 +136,11 @@
                   <span class="ml-1 price">{{ getYourTotal() }}</span>
                 </div>
               </div>
-              <div 
+              <div
                 class="center-container d-flex mx-0 mx-md-auto justify-content-between align-items-center col-md-8 col-xl-12"
                 :class="{'center-cont-height':(lastSubmittedOffer.theirs_items.length > ONE_ITEM || lastSubmittedOffer.yours_items.length) }"
               >
-                <div class="left-item">      
+                <div class="left-item">
                   <div v-for="(item, index) in lastSubmittedOffer.theirs_items" :id="lastSubmittedOffer.theirs_items.length === THREE_ITEMS ?'trade-item-'+index : ''"
                       :key="item.id" class="item"
                       :class="[((lastSubmittedOffer.theirs_items.length > ONE_ITEM )|| (lastSubmittedOffer.yours_items.length)) ? 'item-length' : 'item-normal']">
@@ -154,7 +154,7 @@
                         <div>, {{ $t('trades.trade_arena.size') }} {{ item.inventory.size.size }}</div>
                       </div>
                       <span class="mt-1 item-caption-description">
-                        {{ $t('trades.trade_arena.box') }}: 
+                        {{ $t('trades.trade_arena.box') }}:
                         {{ item.inventory.packaging_condition.name }}
                       </span>
                     </div>
@@ -182,7 +182,7 @@
                           <div>, {{ $t('trades.trade_arena.size') }} {{ item.inventory.size.size }}</div>
                         </div>
                         <span class="mt-1 item-caption-description">
-                          {{ $t('trades.trade_arena.box') }}: 
+                          {{ $t('trades.trade_arena.box') }}:
                           {{ item.inventory.packaging_condition.name }}
                         </span>
                       </div>
@@ -297,7 +297,7 @@ export default {
         this.$router.push('/profile/trades/dashboard')
     })
 
-    document.querySelector('.main-wrapper').style.backgroundColor = '#F7F7F7' 
+    document.querySelector('.main-wrapper').style.backgroundColor = '#F7F7F7'
 
     this.fetchOfferDetails()
     this.width = window.innerWidth
@@ -432,7 +432,7 @@ export default {
   @include body-7-bold
   font-family: $font-family-sf-pro-display
   color: $color-black-1
-  
+
 .main-content
   padding: 50px 20px
   @media (min-width: 768px)
@@ -461,10 +461,10 @@ export default {
   font-weight: $medium
   font-size: 14px
   color: $color-black-1
-  height: 35px
+  height: 23px
   background: $color-gray-1
-  padding-top: 7px
-  padding-left: 30px
+  width: 99px
+  text-align: center
 .fair-trade-division-mobile
   background-color: $color-white-4
   width: 247px
@@ -689,4 +689,13 @@ export default {
 
 .image-small-size
   background: $color-gray-1
+.responsive-div
+  background: $color-white-1
+.overlay-mob
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  background: $color-grey-70
 </style>
