d0e53aef69fa2f799f5e0fb6a685e63139e64bc6
Mustafa Demir <demir.dev@deadstock.co>
2022-11-16 03:19:47 +0300

    feat: new search mobile page design

---
+++ components/MobileSearchCarousel.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="product-carousel-wrapper">
+    <client-only>
+      <Carousel
+        v-if="items.length"
+        ref="carousel"
+        :loop="loop"
+        :nav="false"
+        :showArrows="showArrows"
+        :center="true"
+        :margin="0"
+        :responsive="responsiveAttr"
+        :mouse-drag="true"
+        :dots="false"
+        class="carousel"
+      >
+        <template #default>
+          <slot name="product">
+            <div
+              v-for="(item, index) in items"
+              :key="`product-carousel-${index}`"
+              :class="{ item: true, 'photo-item': variant === 'photo' }"
+            >
+              <div v-if="variant === 'trade' || variant === 'auctions'">
+                <CountdownItem :time="60000" :key="index" />
+                <ProductThumb :src="item.image" :img-class="'m-0'" />
+                <div class="title mt-2">{{ item.name }}</div>
+                <div class="color text-gray-light fs-14 fw-5 font-secondary">{{ item.color }}</div>
+
+                <div v-if="variant === 'trade'" class="d-flex justify-content-between">
+                  <div class="font-secondary fs-14 mt-1 text-black body-5-normal">
+                    {{ item.size }}
+                  </div>
+                  <a role="button" class="trade d-flex align-items-center">
+                    <img src="~/assets/img/mobile-search/arrows.svg" /> <span class="ml-2">{{ $t('mobile_search.trade')}}</span>
+                  </a>
+                </div>
+
+                <div v-else class="d-flex justify-content-sm-start w-100">
+                 <div class="font-secondary fs-14 mt-1 text-black body-5-normal">
+                   &dollar;{{ item.price | formatPrice }}
+                 </div>
+                  <div class="d-flex justify-content-between w-100">
+                     <span class="font-secondary fs-12 mt-1 text-gray-light body-5-light px-1 flex-grow-1">
+                        ({{ item.size }})
+                     </span>
+                    <a role="button" class="bid d-flex align-items-center justify-content-between">
+                      <span>{{ $t('mobile_search.bid_now')}}</span>
+                    </a>
+                  </div>
+
+               </div>
+              </div>
+              <div v-else>
+                <ProductThumb :src="item.image" :img-class="'m-0'" />
+                <div class="title mt-2">{{ item.title }}</div>
+              </div>
+            </div>
+          </slot>
+        </template>
+        <template #prev>
+          <div class="owl-nav owl-prev">
+            <img :src="require('~/assets/img/home/arrow-left.svg')" />
+          </div>
+        </template>
+
+        <template #next>
+          <div class="owl-nav owl-next">
+            <img :src="require('~/assets/img/home/arrow-right.svg')" />
+          </div>
+        </template>
+      </Carousel>
+
+      <div v-else class="no-text py-5">
+        {{ $t('message.no_products_found') }}
+      </div>
+    </client-only>
+  </div>
+</template>
+<script>
+import ProductThumb from '~/components/mobile-search/Thumb.vue'
+import CountdownItem from '~/components/mobile-search/CountdownItem'
+
+export default {
+  name: 'MobileSearchCarousel',
+  components: {CountdownItem, ProductThumb },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    variant: {
+      type: String, // enum: ['detail', 'photo', 'countdown']
+      default: 'detail',
+    },
+    loop: {
+      type: Boolean,
+      default: false,
+    },
+    pageName: {
+      type: String,
+      default: 'shop',
+    },
+    showArrows: {
+      type: Boolean,
+      default: false,
+    },
+    itemSettings: {
+      type: Object,
+      default: () => null
+    },
+  },
+  computed: {
+    responsiveAttr() {
+      if(this.itemSettings) return this.itemSettings
+      if (this.variant === 'detail') {
+        return {
+          0: { items: 3, nav: false, center: false },
+          768: { items: 4, nav: false, center: false },
+          950: { items: 5, nav: false, center: false },
+        }
+      } else {
+        return {
+          0: { items: 3, nav: false, center: false },
+          768: { items: 4, nav: false, center: false },
+          950: { items: 5, nav: false, center: false },
+        }
+      }
+    },
+  },
+
+  watch: {
+    items() {
+      // Destroy and recreate carousel when products change
+      this.$refs.carousel?.destroy()
+      this.$nextTick(() => this.$refs.carousel?.create())
+    },
+  },
+}
+</script>
+<style lang="sass" scoped>
+@import '~/assets/css/_variables'
+.product-carousel-wrapper
+  padding: 0
+  .no-text
+    @include body-4-medium
+    text-align: center
+    color: $color-gray-5
+  .carousel::v-deep
+    width: 100%
+    text-align: center
+    position: relative
+    margin-left: auto
+    margin-right: auto
+    display: flex
+    align-items: center
+    @media (max-width: 576px)
+      [id^='carousel_next_']
+        display: none
+      [id^='carousel_prev_']
+        display: none
+    >span .owl-nav
+      display: block
+    .owl-carousel
+      .item
+        text-align: left
+        margin: 0
+        margin-left: 0.5em
+        margin-right: 0.5em
+        &.photo-item
+          img
+            object-fit: contain
+            @media (max-width: 500px)
+              //height: 300px
+    .owl-nav
+      width: auto
+      cursor: pointer
+      [class*='owl-']
+        background: none
+      &.owl-next
+        float: right
+        margin-right: -15px
+      &.owl-prev
+        float: left
+        margin-left: -15px
+
+  .title
+    font-family: $font-family-sf-pro-display
+    @include body-4-medium
+
+  .color
+    font-family: $font-family-sf-pro-display
+
+  .trade
+    font-family: $font-montserrat
+    @include body-18-normal
+    background-color: $color-gray-73
+    color: $color-white
+    border-radius: 4px
+    padding: 5px 8px
+    img
+      width: 13px
+      height: 9px
+
+  .bid
+    font-family: $font-montserrat
+    @include body-18-normal
+    background-color: $color-black-1
+    color: $color-white
+    padding: 5px 8px
+</style>
---
+++ pages/mobile-search/index.vue
@@ -0,0 +1,229 @@
+<template>
+  <div class="container-search pb-5">
+    <section>
+      <div class="container">
+        <div class="d-flex align-items-center">
+          <Searchbar :placeholder="$t('common.search')" class="flex-grow-1" />
+          <a role="button">
+            <img src="~/assets/img/icons/filter-icon.png" />
+          </a>
+        </div>
+      </div>
+    </section>
+    <div v-if="!noSearchResult" class="container">
+      <section class="mt-5">
+        <SectionHeader class="px-2"
+          :title="$t('common.sizetype')"
+          full-width
+        />
+        <MobileSearchCarousel
+          class="mt-4 mb-5"
+          :items="sizeType"
+        />
+      </section>
+      <section class="featured">
+        <SectionHeader class="px-2"
+          :title="$t('preferences.payments.featured')"
+          full-width
+        />
+        <MobileSearchCarousel
+          class="mt-4 mb-5"
+          :items="featured"
+          variant="photo"
+          :item-settings="{
+            0: { items: 2, nav: false, center: false },
+          }"
+                         loop />
+      </section>
+      <section class="shop_by_style">
+        <SectionHeader class="px-2"
+          :title="$t('shop_by_style.title')"
+          :label="$t('banner.view_more_trending')"
+          to="/url/here"
+          full-width
+        />
+        <div class="px-2 mt-4 mb-5">
+          <a><img :src="require('~/assets/img/mobile-search/created_for_you.png')" class="w-100" /></a>
+        </div>
+      </section>
+      <section class="shop_by_category">
+        <SectionHeader class="px-2"
+          :title="$t('mobile_search.shop_by_category')"
+          full-width
+          loop
+          to="/url/here"
+        />
+        <MobileSearchCarousel
+          class="mt-4 mb-5"
+          :items="shopByCategory"
+          variant="photo"
+          :item-settings="{
+            0: { items: 2, nav: false, center: true },
+          }"
+          loop />
+      </section>
+      <section class="shop_by_brand">
+        <SectionHeader class="px-2"
+          :title="$t('mobile_search.shop_by_brand')"
+          to="/url/here"
+          full-width
+        />
+        <div class="px-2 mt-4 mb-5">
+          <a><img :src="require('~/assets/img/mobile-search/shop_by_brand.png')" class="w-100" /></a>
+        </div>
+      </section>
+      <section class="view_trades">
+        <SectionHeader class="px-2"
+          :title="$t('mobile_search.view_trades')"
+          to="/url/here"
+          full-width
+        />
+        <MobileSearchCarousel
+          class="mt-4 mb-5"
+          :items="viewTrades"
+          :variant="'trade'"
+          :item-settings="{
+            0: { items: 2, nav: false, center: false }
+          }"
+          loop />
+      </section>
+      <section class="view_auctions">
+        <SectionHeader class="px-2"
+          :title="$t('mobile_search.view_auctions')"
+          to="/url/here"
+          full-width
+        />
+        <MobileSearchCarousel
+          class="mt-4 mb-5"
+          :items="auctions"
+          :variant="'auctions'"
+          :item-settings="{
+            0: { items: 2, nav: false, center: false }
+          }"
+          loop />
+      </section>
+    </div>
+    <div v-else>
+      <div class="text-center no-items-found">
+        <img src="~/assets/img/no-items-found.png" class="mr-3" />
+        <div>
+          <div class="no-items-found-title">
+            {{ $t('auctions.frontpage.no_results_found') }}
+          </div>
+          <div class="no-items-found-subtitle">
+            {{ $t('auctions.frontpage.cant_find_anything') }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Searchbar from '~/components/mobile-search/Searchbar'
+import MobileSearchCarousel from '~/components/MobileSearchCarousel'
+export default {
+  name: 'Index',
+  components: {
+    Searchbar, MobileSearchCarousel
+  },
+  data() {
+    return {
+      noSearchResult: false,
+      sizeType: [
+        { image: require('~/assets/img/mobile-search/img-1.png'), title: 'Men' },
+        { image: require('~/assets/img/mobile-search/img-2.png'), title: 'Women' },
+        { image: require('~/assets/img/mobile-search/img-3.png'), title: 'Kids' }
+      ],
+      featured: [
+        { image: require('~/assets/img/mobile-search/featured_1.png'), title: 'New Releases' },
+        { image: require('~/assets/img/mobile-search/featured_2.png'), title: 'Instant Shipping' },
+        { image: require('~/assets/img/mobile-search/featured_3.png'), title: 'Best Selling' },
+        { image: require('~/assets/img/mobile-search/featured_4.png'), title: 'Trending' },
+        { image: require('~/assets/img/mobile-search/featured_5.png'), title: 'Gift Card' },
+        { image: require('~/assets/img/mobile-search/featured_6.png'), title: '' }
+      ],
+      shopByStyle: [
+        { image: require('~/assets/img/mobile-search/created_for_you.png'), title: '' },
+      ],
+      shopByCategory: [
+        { image: require('~/assets/img/mobile-search/category_1.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_2.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_3.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_4.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_5.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_6.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_7.png'), title: '' },
+        { image: require('~/assets/img/mobile-search/category_8.png'), title: '' },
+      ],
+      viewTrades: [
+        {
+          name: 'Nike Air Force',
+          image: 'https://images.deadstock.co/products/sneakers/DJ7998-100/800xAUTO/IMG01.jpg',
+          timer: '5d 18h',
+          color: 'University Blue',
+          size: 'Size 9',
+          price: '345,00'
+        },
+        {
+          name: 'Nike Air Force',
+          image: 'https://images.deadstock.co/products/sneakers/DJ7998-100/800xAUTO/IMG01.jpg',
+          timer: '5d 18h',
+          color: 'University Blue',
+          size: 'Size 9',
+          price: '345,00'
+        },
+      ],
+      auctions: [
+        {
+          name: 'Nike Air Force',
+          image: 'https://images.deadstock.co/products/sneakers/DJ7998-100/800xAUTO/IMG01.jpg',
+          timer: '5d 18h',
+          color: 'University Blue',
+          size: 'Size 9',
+          price: '345,00'
+        },
+        {
+          name: 'Nike Air Force',
+          image: 'https://images.deadstock.co/products/sneakers/DJ7998-100/800xAUTO/IMG01.jpg',
+          timer: '5d 18h',
+          color: 'University Blue',
+          size: 'Size 9',
+          price: '345,00'
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/css/_variables'
+.container-search
+  @media (min-width: 1024px)
+    .scroll-to-top
+      display: flex !important
+      bottom: -100px
+      right: 60px
+      border: 2px solid $color-blue-20
+      width: 40px
+      aspect-ratio: 1
+      z-index: 1020
+      transition: 0.5s all ease-in-out
+      &.scrolled
+        bottom: 40px
+  .no-items-found
+    img
+      width: 130px
+    &-title
+      @include heading-7
+      color: $color-gray-5
+      font-family: $font-sp-pro
+    &-subtitle
+      @include body-1-regular
+      font-family: $font-sp-pro
+      color: $black
+::v-deep .section-header
+  @media (max-width: 576px)
+    margin: 30px 12.5px
+
