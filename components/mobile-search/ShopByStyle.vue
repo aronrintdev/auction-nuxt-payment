cf95bd1891e3d6dac1ab6d941bc56d9b5045b107
Mustafa Demir <demir.dev@deadstock.co>
2022-11-20 20:30:06 +0300

    #DVQ-588 - fixed

--- components/mobile-search/ShopByStyle.vue
+++ components/mobile-search/ShopByStyle.vue
@@ -3,7 +3,6 @@
     <SectionHeader class="px-2"
                    :title="$t('shop_by_style.title')"
                    :label="$t('banner.view_more_trending')"
-                   to="/shop-by-style"
                    full-width
     />
     <div class="px-2 mt-4 mb-5">
