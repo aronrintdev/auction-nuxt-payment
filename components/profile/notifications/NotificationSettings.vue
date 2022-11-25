774926471e44254e8a7c56f4d4af44f8095f1aca
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2022-11-25 22:30:47 +0400

    Fix: Lint

--- components/profile/notifications/NotificationSettings.vue
+++ components/profile/notifications/NotificationSettings.vue
@@ -1,64 +1,78 @@
 <template>
-  <div :class="{
-    'mt-4': !isScreenXS,
-    'm-2': isScreenXS
-  }">
-    <div v-if="loading && getSettings.length === 0" class="d-flex align-items-center justify-content-center">
+  <div
+    :class="{
+      'mt-4': !isScreenXS,
+      'm-2': isScreenXS,
+    }"
+  >
+    <div
+      v-if="loading && getSettings.length === 0"
+      class="d-flex align-items-center justify-content-center"
+    >
       <Loader :loading="loading"></Loader>
     </div>
     <div v-else>
       <NotificationSettingsSection
-          :items="settings()"
-          :path="'all'"
-          :preference="false"
-          :title="$t('notifications.settings.all_communication')">
+        :items="settings()"
+        :path="'all'"
+        :preference="false"
+        :title="$t('notifications.settings.all_communication')"
+      >
         <template #extra>
           <div v-if="isScreenXS">
-            <ItemDivider/>
-            <div class="d-flex align-items-center ">
+            <ItemDivider />
+            <div class="d-flex align-items-center">
               <div>
-                <div :class="{'justify-content-between': isScreenXS}" class="push-title d-flex align-items-center ">
+                <div
+                  :class="{ 'justify-content-between': isScreenXS }"
+                  class="push-title d-flex align-items-center"
+                >
                   {{ $t('notifications.push.title') }}
                 </div>
-                <div class="push-sub-title mt-2 mr-2">{{ $t('notifications.push.desc') }}</div>
+                <div class="push-sub-title mt-2 mr-2">
+                  {{ $t('notifications.push.desc') }}
+                </div>
               </div>
-              <NotificationSwitch :value="pushActive"
-                                  class="mr-3"
-                                  @change="activeChange"/>
+              <NotificationSwitch
+                :value="pushActive"
+                class="mr-3"
+                @change="activeChange"
+              />
             </div>
           </div>
         </template>
       </NotificationSettingsSection>
 
       <div class="d-flex justify-content-center my-4">
-        <NavGroup v-if="isVendor"
-            :data="tabs"
-            :value="currentTab"
-            nav-key="notification-tabs"
-                  class="notification-tabs"
-            @change="handlePageChange"
+        <NavGroup
+          v-if="isVendor"
+          :data="tabs"
+          :value="currentTab"
+          nav-key="notification-tabs"
+          class="notification-tabs"
+          @change="handlePageChange"
         />
       </div>
 
       <div>
-        <NotificationSettingsTab :tab="currentTab"/>
+        <NotificationSettingsTab :tab="currentTab" />
       </div>
 
       <div v-if="!isScreenXS" class="mt-5">
         <Button
-            :disabled="loading || changedSettings.length === 0"
-            class="mr-2"
-            pill
-            variant="dark-blue"
-            @click="saveChanges"
-        >{{ $t('common.save_changes') }}
+          :disabled="loading || changedSettings.length === 0"
+          class="mr-2"
+          pill
+          variant="dark-blue"
+          @click="saveChanges"
+          >{{ $t('common.save_changes') }}
         </Button>
         <Button
-            :disabled="loading"
-            pill
-            variant="outline-dark"
-            @click="updateSettings"
-        >{{ $t('common.discard_changes') }}
+          :disabled="loading"
+          pill
+          variant="outline-dark"
+          @click="updateSettings"
+          >{{ $t('common.discard_changes') }}
         </Button>
       </div>
     </div>
@@ -66,22 +80,27 @@
 </template>
 
 <script>
-import {mapActions, mapGetters} from 'vuex';
-import NotificationSettingsSection from '~/components/profile/notifications/NotificationSettingsSection';
-import NavGroup from '~/components/common/NavGroup';
-import NotificationSettingsTab from '~/components/profile/notifications/NotificationSettingsTab';
-import {ALL_SETTINGS} from '~/static/constants/notifications';
-import {Button} from '~/components/common';
-import Loader from '~/components/common/Loader';
-import screenSize from '~/plugins/mixins/screenSize';
-import ItemDivider from '~/components/profile/notifications/ItemDivider';
-import NotificationSwitch from '~/components/profile/notifications/Switch';
+import { mapActions, mapGetters } from 'vuex'
+import NotificationSettingsSection from '~/components/profile/notifications/NotificationSettingsSection'
+import NavGroup from '~/components/common/NavGroup'
+import NotificationSettingsTab from '~/components/profile/notifications/NotificationSettingsTab'
+import { ALL_SETTINGS } from '~/static/constants/notifications'
+import { Button } from '~/components/common'
+import Loader from '~/components/common/Loader'
+import screenSize from '~/plugins/mixins/screenSize'
+import ItemDivider from '~/components/profile/notifications/ItemDivider'
+import NotificationSwitch from '~/components/profile/notifications/Switch'
 
 export default {
   name: 'NotificationSettings',
   components: {
     NotificationSwitch,
-    ItemDivider, Loader, NotificationSettingsTab, Button, NavGroup, NotificationSettingsSection
+    ItemDivider,
+    Loader,
+    NotificationSettingsTab,
+    Button,
+    NavGroup,
+    NotificationSettingsSection,
   },
   mixins: [screenSize],
   data() {
@@ -98,27 +117,30 @@ export default {
           label: this.$t('notifications.settings.tabs.selling'),
           value: 'selling',
         },
-      ]
+      ],
     }
   },
   computed: {
     ...mapGetters({
-      'changedSettings': 'notifications/getChangedSettings',
-      'getSettings': 'notifications/getSettings',
-      'pushActive': 'notifications/getPushNotificationsActive',
+      changedSettings: 'notifications/getChangedSettings',
+      getSettings: 'notifications/getSettings',
+      pushActive: 'notifications/getPushNotificationsActive',
       isVendor: 'auth/isVendor',
-    })
+    }),
   },
   methods: {
     ...mapActions({
-      'fetchSettings': 'notifications/getUserSettings',
-      'bulkEditNotificationSettings': 'notifications/bulkEditNotificationSettings',
+      fetchSettings: 'notifications/getUserSettings',
+      bulkEditNotificationSettings:
+        'notifications/bulkEditNotificationSettings',
     }),
     handlePageChange(page) {
       this.currentTab = page
     },
     settings() {
-      return Object.keys(ALL_SETTINGS.communication).map(key => ALL_SETTINGS.communication[key])
+      return Object.keys(ALL_SETTINGS.communication).map(
+        (key) => ALL_SETTINGS.communication[key]
+      )
     },
     updateSettings() {
       this.loading = true
@@ -133,16 +155,19 @@ export default {
     },
     saveChanges() {
       this.loading = true
-      this.bulkEditNotificationSettings({settings: this.changedSettings}).then(res => {
-        this.updateSettings()
-        this.$toasted.success(res.data.message)
-      }).catch(err => {
-        this.$toasted.error(err.response.data.message)
-      }).finally(() => {
-        this.loading = false
-      })
-    }
-  }
+      this.bulkEditNotificationSettings({ settings: this.changedSettings })
+        .then((res) => {
+          this.updateSettings()
+          this.$toasted.success(res.data.message)
+        })
+        .catch((err) => {
+          this.$toasted.error(err.response.data.message)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+  },
 }
 </script>
 
