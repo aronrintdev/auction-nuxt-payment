2e913868c9fd87bc3591eab8742ceadb4657f460
aronrint <aronrint@gmail.com>
2022-12-01 00:37:02 +0200

    Checkout selling index page
    - Updated the Checkout selling index page and added the emitting of the hide footer event.

--- pages/checkout/selling/index.vue
+++ pages/checkout/selling/index.vue
@@ -38,6 +38,7 @@ export default {
   },
   beforeMount() {
     this.$root.$emit('hide-header', { hideHeader: true })
+    this.$root.$emit('hide-footer', { hideFooter: true })
 
     if (this.$store.$auth.loggedIn) {
       this.getUserDetails()
@@ -55,6 +56,7 @@ export default {
   },
   beforeDestroy() {
     this.$root.$emit('hide-header', { hideHeader: false })
+    this.$root.$emit('hide-footer', { hideFooter: false })
   },
   methods: {
     ...mapActions({
