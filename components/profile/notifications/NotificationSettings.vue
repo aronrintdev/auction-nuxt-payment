fa382cd043588503fed2655d6582704deabebc55
Roshaan Mustafa <114186393+mustafa-deadstock@users.noreply.github.com>
2022-12-06 18:31:56 +0500

    padding spacing changes update

--- components/profile/notifications/NotificationSettings.vue
+++ components/profile/notifications/NotificationSettings.vue
@@ -1,30 +1,44 @@
 <template>
-  <div :class="{
-    'mt-4': !isScreenXS,
-    'm-2': isScreenXS
-  }">
-    <div v-if="loading && getSettings.length === 0" class="d-flex align-items-center justify-content-center">
+  <div
+    :class="{
+      'mt-4': !isScreenXS,
+      'm-2': isScreenXS,
+    }"
+    class="notification-setting-main"
+  >
+    <div
+      v-if="loading && getSettings.length === 0"
+      class="d-flex align-items-center justify-content-center"
+    >
       <Loader :loading="loading"></Loader>
     </div>
     <div v-else>
       <NotificationSettingsSection
-          :items="settings()"
-          :path="'all'"
-          :preference="false"
-          :title="$t('notifications.settings.all_communication')">
+        :items="settings()"
+        :path="'all'"
+        :preference="false"
+        :title="$t('notifications.settings.all_communication')"
+      >
         <template #extra>
           <div v-if="isScreenXS">
-            <ItemDivider/>
-            <div class="d-flex align-items-center ">
+            <ItemDivider />
+            <div class="d-flex align-items-center">
               <div>
-                <div :class="{'justify-content-between': isScreenXS}" class="push-title d-flex align-items-center ">
+                <div
+                  :class="{ 'justify-content-between': isScreenXS }"
+                  class="push-title d-flex align-items-center"
+                >
                   {{ $t('notifications.push.title') }}
                 </div>
-                <div class="push-sub-title mt-2 mr-2">{{ $t('notifications.push.desc') }}</div>
+                <div class="push-sub-title mt-2 mr-2">
+                  {{ $t('notifications.push.desc') }}
+                </div>
               </div>
-              <NotificationSwitch :value="pushActive"
-                                  class="mr-3"
-                                  @change="activeChange"/>
+              <NotificationSwitch
+                :value="pushActive"
+                class="mr-3"
+                @change="activeChange"
+              />
             </div>
           </div>
         </template>
@@ -32,32 +46,33 @@
 
       <div class="text-center my-4">
         <NavGroup
-            :data="tabs"
-            :value="currentTab"
-            nav-key="notification-tabs"
-            @change="handlePageChange"
+          :data="tabs"
+          :value="currentTab"
+          nav-key="notification-tabs"
+          class="notification-setting-nav"
+          @change="handlePageChange"
         />
       </div>
 
       <div>
-        <NotificationSettingsTab :tab="currentTab"/>
+        <NotificationSettingsTab :tab="currentTab" />
       </div>
 
-      <div v-if="!isScreenXS" class="mt-5">
+      <div v-if="!isScreenXS" class="mt-5 buttons-sec">
         <Button
-            :disabled="loading || changedSettings.length === 0"
-            class="mr-2"
-            pill
-            variant="blue"
-            @click="saveChanges"
-        >{{ $t('common.save_changes') }}
+          :disabled="loading || changedSettings.length === 0"
+          class="mr-2"
+          pill
+          variant="blue"
+          @click="saveChanges"
+          >{{ $t('common.save_changes') }}
         </Button>
         <Button
-            :disabled="loading"
-            pill
-            variant="outline-dark"
-            @click="updateSettings"
-        >{{ $t('common.discard_changes') }}
+          :disabled="loading"
+          pill
+          variant="outline-dark"
+          @click="updateSettings"
+          >{{ $t('common.discard_changes') }}
         </Button>
       </div>
     </div>
@@ -65,22 +80,27 @@
 </template>
 
 <script>
-import {mapActions, mapGetters} from 'vuex';
-import NotificationSettingsSection from '~/components/profile/notifications/NotificationSettingsSection';
-import NavGroup from '~/components/common/NavGroup';
-import NotificationSettingsTab from '~/components/profile/notifications/NotificationSettingsTab';
-import {ALL_SETTINGS} from '~/static/constants/notifications';
-import {Button} from '~/components/common';
-import Loader from '~/components/common/Loader';
-import screenSize from '~/plugins/mixins/screenSize';
-import ItemDivider from '~/components/profile/notifications/ItemDivider';
-import NotificationSwitch from '~/components/profile/notifications/Switch';
+import { mapActions, mapGetters } from 'vuex'
+import NotificationSettingsSection from '~/components/profile/notifications/NotificationSettingsSection'
+import NavGroup from '~/components/common/NavGroup'
+import NotificationSettingsTab from '~/components/profile/notifications/NotificationSettingsTab'
+import { ALL_SETTINGS } from '~/static/constants/notifications'
+import { Button } from '~/components/common'
+import Loader from '~/components/common/Loader'
+import screenSize from '~/plugins/mixins/screenSize'
+import ItemDivider from '~/components/profile/notifications/ItemDivider'
+import NotificationSwitch from '~/components/profile/notifications/Switch'
 
 export default {
   name: 'NotificationSettings',
   components: {
     NotificationSwitch,
-    ItemDivider, Loader, NotificationSettingsTab, Button, NavGroup, NotificationSettingsSection
+    ItemDivider,
+    Loader,
+    NotificationSettingsTab,
+    Button,
+    NavGroup,
+    NotificationSettingsSection,
   },
   mixins: [screenSize],
   data() {
@@ -97,26 +117,29 @@ export default {
           label: this.$t('notifications.settings.tabs.selling'),
           value: 'selling',
         },
-      ]
+      ],
     }
   },
   computed: {
     ...mapGetters({
-      'changedSettings': 'notifications/getChangedSettings',
-      'getSettings': 'notifications/getSettings',
-      'pushActive': 'notifications/getPushNotificationsActive'
-    })
+      changedSettings: 'notifications/getChangedSettings',
+      getSettings: 'notifications/getSettings',
+      pushActive: 'notifications/getPushNotificationsActive',
+    }),
   },
   methods: {
     ...mapActions({
-      'fetchSettings': 'notifications/getUserSettings',
-      'bulkEditNotificationSettings': 'notifications/bulkEditNotificationSettings',
+      fetchSettings: 'notifications/getUserSettings',
+      bulkEditNotificationSettings:
+        'notifications/bulkEditNotificationSettings',
     }),
     handlePageChange(page) {
       this.currentTab = page
     },
     settings() {
-      return Object.keys(ALL_SETTINGS.communication).map(key => ALL_SETTINGS.communication[key])
+      return Object.keys(ALL_SETTINGS.communication).map(
+        (key) => ALL_SETTINGS.communication[key]
+      )
     },
     updateSettings() {
       this.loading = true
@@ -131,21 +154,30 @@ export default {
     },
     saveChanges() {
       this.loading = true
-      this.bulkEditNotificationSettings({settings: this.changedSettings}).then(res => {
-        this.updateSettings()
-        this.$toasted.success(res.data.message)
-      }).catch(err => {
-        this.$toasted.error(err.response.data.message)
-      }).finally(() => {
-        this.loading = false
-      })
-    }
-  }
+      this.bulkEditNotificationSettings({ settings: this.changedSettings })
+        .then((res) => {
+          this.updateSettings()
+          this.$toasted.success(res.data.message)
+        })
+        .catch((err) => {
+          this.$toasted.error(err.response.data.message)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+  },
 }
 </script>
 
 <style lang="sass" scoped>
 @import "~/assets/css/variables"
+.notification-setting-main
+  .notification-setting-nav
+    margin: 26px 0 18px 0 !important
+  .buttons-sec
+    button
+      width: 217px
 
 .push-title
   @include body-13
--- pages/profile/notifications/index.vue
+++ pages/profile/notifications/index.vue
@@ -1,24 +1,38 @@
 <template>
   <client-only>
-    <div :class="{
+    <div
+      :class="{
         'p-5': !isScreenXS,
-        'mobile': isScreenXS,
+        mobile: isScreenXS,
       }"
-         class="profile-notification mb-2">
+      class="profile-notification mb-2"
+    >
       <MobileHeader v-if="isScreenXS && !onSettingsItemTab" :title="tabTitle">
         <template #actions>
           <div class="d-flex align-items-center">
-            <filter-svg v-if="onNotifications" class="mr-3" role="button"
-                        @click="mobileFiltersOpen = !mobileFiltersOpen"></filter-svg>
-            <setting-svg v-if="onNotifications" role="button" @click="handlePageChange('Settings')"></setting-svg>
-            <close-svg v-if="onSettings" role="button" @click="handlePageChange('Notifications')"></close-svg>
+            <filter-svg
+              v-if="onNotifications"
+              class="mr-3"
+              role="button"
+              @click="mobileFiltersOpen = !mobileFiltersOpen"
+            ></filter-svg>
+            <setting-svg
+              v-if="onNotifications"
+              role="button"
+              @click="handlePageChange('Settings')"
+            ></setting-svg>
+            <close-svg
+              v-if="onSettings"
+              role="button"
+              @click="handlePageChange('Notifications')"
+            ></close-svg>
           </div>
         </template>
         <template #expanded-content>
           <div v-if="onNotifications" class="d-flex flex-column">
-            <MobileSearchInput @input="searchChanged"/>
+            <MobileSearchInput @input="searchChanged" />
             <div class="d-flex align-items-center justify-content-end">
-              <NotificationMarkAllAsRead/>
+              <NotificationMarkAllAsRead />
             </div>
           </div>
         </template>
@@ -26,40 +40,48 @@
       <div v-if="onSettings && !isScreenXS" class="title">
         {{ tabTitle }}
       </div>
-      <NotificationFilters v-if="onNotifications && !isScreenXS" @filter="filtersChanged"/>
-      <NotificationMobileFilters v-if="onNotifications && isScreenXS" :open="mobileFiltersOpen"
-                                 @closed="mobileFiltersOpen = false" @filter="filtersChanged"/>
+      <NotificationFilters
+        v-if="onNotifications && !isScreenXS"
+        @filter="filtersChanged"
+      />
+      <NotificationMobileFilters
+        v-if="onNotifications && isScreenXS"
+        :open="mobileFiltersOpen"
+        @closed="mobileFiltersOpen = false"
+        @filter="filtersChanged"
+      />
       <div v-if="!isScreenXS && !onSettingsItemTab" class="text-center mt-4">
         <NavGroup
-            :data="tabs"
-            :value="currentTab"
-            nav-key="notification-tabs"
-            @change="handlePageChange"
+          :data="tabs"
+          :value="currentTab"
+          nav-key="notification-tabs"
+          class="notification-nav"
+          @change="handlePageChange"
         />
       </div>
-      <NotificationsTab v-if="onNotifications" @infinite="infiniteScroll"/>
-      <NotificationSettings v-if="onSettings"/>
-      <MobileNotificationSettingsTab v-if="onSettingsItemTab"/>
+      <NotificationsTab v-if="onNotifications" @infinite="infiniteScroll" />
+      <NotificationSettings v-if="onSettings" />
+      <MobileNotificationSettingsTab v-if="onSettingsItemTab" />
     </div>
   </client-only>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
-import {NavGroup} from '~/components/common';
-import NotificationFilters from '~/components/profile/notifications/NotificationFilters';
-import NotificationsTab from '~/components/profile/notifications/NotificationsTab';
-import NotificationSettings from '~/components/profile/notifications/NotificationSettings';
-import screenSize from '~/plugins/mixins/screenSize';
-import MobileHeader from '~/components/mobile/MobileHeader';
+import { mapGetters } from 'vuex'
+import { NavGroup } from '~/components/common'
+import NotificationFilters from '~/components/profile/notifications/NotificationFilters'
+import NotificationsTab from '~/components/profile/notifications/NotificationsTab'
+import NotificationSettings from '~/components/profile/notifications/NotificationSettings'
+import screenSize from '~/plugins/mixins/screenSize'
+import MobileHeader from '~/components/mobile/MobileHeader'
 import settingSvg from '~/assets/img/profile/notifications/settings.svg?inline'
 import filterSvg from '~/assets/img/profile/notifications/filters.svg?inline'
 import closeSvg from '~/assets/img/icons/close.svg?inline'
-import NotificationMarkAllAsRead from '~/components/profile/notifications/NotificationMarkAllAsRead';
-import MobileSearchInput from '~/components/mobile/MobileSearchInput';
-import NotificationMobileFilters from '~/components/profile/notifications/NotificationMobileFilters';
-import MobileNotificationSettingsTab from '~/components/profile/notifications/MobileNotificationSettingsTab';
-import {NOTIFICATION_PER_PAGE} from '~/static/constants';
+import NotificationMarkAllAsRead from '~/components/profile/notifications/NotificationMarkAllAsRead'
+import MobileSearchInput from '~/components/mobile/MobileSearchInput'
+import NotificationMobileFilters from '~/components/profile/notifications/NotificationMobileFilters'
+import MobileNotificationSettingsTab from '~/components/profile/notifications/MobileNotificationSettingsTab'
+import { NOTIFICATION_PER_PAGE } from '~/static/constants'
 
 export default {
   name: 'Notifications',
@@ -68,7 +90,14 @@ export default {
     NotificationMobileFilters,
     MobileSearchInput,
     NotificationMarkAllAsRead,
-    MobileHeader, NotificationSettings, NotificationsTab, NotificationFilters, NavGroup, settingSvg, filterSvg, closeSvg
+    MobileHeader,
+    NotificationSettings,
+    NotificationsTab,
+    NotificationFilters,
+    NavGroup,
+    settingSvg,
+    filterSvg,
+    closeSvg,
   },
   mixins: [screenSize],
   layout: 'Profile',
@@ -91,11 +120,15 @@ export default {
   },
   computed: {
     ...mapGetters({
-      'currentTab': 'notifications/getTab',
-      'total': 'notifications/getTotal',
+      currentTab: 'notifications/getTab',
+      total: 'notifications/getTotal',
     }),
     tabTitle() {
-      return this.onNotifications ? this.$t('notifications.title') : (this.isScreenXS ? this.$t('notifications.push_notifications') : this.$t('notifications.notification_settings'))
+      return this.onNotifications
+        ? this.$t('notifications.title')
+        : this.isScreenXS
+        ? this.$t('notifications.push_notifications')
+        : this.$t('notifications.notification_settings')
     },
     onSettings() {
       return this.currentTab === 'Settings'
@@ -105,14 +138,14 @@ export default {
     },
     onSettingsItemTab() {
       return this.currentTab === 'SettingItemTab'
-    }
+    },
   },
   methods: {
     handlePageChange(page) {
       this.$store.commit('notifications/setTab', page)
     },
     searchChanged(e) {
-      this.filtersChanged({search: e})
+      this.filtersChanged({ search: e })
     },
     infiniteScroll() {
       if (this.total > this.perPage) {
@@ -126,12 +159,15 @@ export default {
         to_year: filters.years ? filters.years[1] : null,
         search: filters.search || null,
         categories: filters.types || null,
-        perPage: this.perPage
+        perPage: this.perPage,
       }
-      this.$store.commit('notifications/setSelectedStatus', filters.status || 'all')
+      this.$store.commit(
+        'notifications/setSelectedStatus',
+        filters.status || 'all'
+      )
       this.$store.dispatch('notifications/filterChange', allFilters)
       this.mobileFiltersOpen = false
-    }
+    },
   },
 }
 </script>
@@ -148,5 +184,6 @@ export default {
     font-family: $font-family-sf-pro-display
     font-style: normal
     font-weight: $medium
-
+  .notification-nav
+    margin: 20px 0 18px 0 !important
 </style>
