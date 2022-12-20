11c482fcedfcaaebdc581750040d2b3231b0a4cf
Babar Saleh Hayat <babar.dev@deadstock.co>
2022-12-20 23:01:51 +0500

    issues fixes

--- pages/profile/trades/dashboard/OfferItems.vue
+++ pages/profile/trades/dashboard/OfferItems.vue
@@ -37,7 +37,7 @@
             :class="{'offer-summary-item': isOfferSummary,'col-4':!isOfferSummary}"
           >
             <div class="d-inline body-section-box w-100 m-1">
-              <div class="d-flex align-items-center justify-content-center position-relative image-container">
+              <div class="d-flex align-items-center justify-content-center position-relative" :class="{'image-container': isOfferSummary}">
                 <div class="thumb-wrapper">
                   <img v-if="item.inventory" :src="item.inventory.product | getProductImageUrl" class="img-fluid" :class="{'pt-4': !isOfferSummary}"/>
                   <img v-else :src="item.product | getProductImageUrl" class="img-fluid" :class="{'pt-4': !isOfferSummary}" />
