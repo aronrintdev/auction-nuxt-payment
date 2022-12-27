d368a17b6d78f69930b7278ea80f08d3ce84b282
atif-deadstock <97760384+atif-deadstock@users.noreply.github.com>
2022-12-27 01:38:17 -0600

    Update confirm.vue

--- pages/profile/create-listing/selling/confirm.vue
+++ pages/profile/create-listing/selling/confirm.vue
@@ -636,7 +636,7 @@ export default {
     @include heading-1
     color: $color-black-1
     font-family: $font-family-sf-pro-display
-    font-size: 24px
+    @include text-24
 
   .table-inventory
     thead
